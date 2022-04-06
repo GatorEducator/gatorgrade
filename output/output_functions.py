74e11e411220a691f241b6a44c962a66f9e4fec2
Garrison Vanzin <54772966+gvanzin-allegheny@users.noreply.github.com>
2022-04-06 15:11:05 -0400

    Remove old files

--- output/output_functions.py
+++
@@ -1,2 +0,0 @@
-# This class is used for storing the main functions requested from the Github Issue Tracker for the output team.
-# For instance, functions dealing with percentage output, description output, and colorization of text.
