5f060f891bbf9c5f33c1ed9f2cb114ac79a07e69
Garrison Vanzin <54772966+gvanzin-allegheny@users.noreply.github.com>
2022-04-15 11:40:33 -0400

    Add directory tool

--- gatorgrade/output/output_tools.py
+++ gatorgrade/output/output_tools.py
@@ -1,3 +1,11 @@
 # This file is used for storing supporting functions and tools that are used by the methods in output_functions.
 # The main use of this class is to de-clutter output_functions.py for better readability.
 # One example of a function to be written here is the splitting of strings retrieved from the input team, at spaces, into lists.
+
+def get_directory_and_file():
+     formatted_command.append('--file')
+                formatted_command.append(file_name)
+            if "--directory" not in formated_command:
+                formatted_command.append('--file')
+                formatted_command.append(file_name)
+            split_commands.append(fomatted_command)
