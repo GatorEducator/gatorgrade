1ce9fbf7b32e2207afb51b61cd19f632ddc45c66
burgess01 <burgess01@allegheny.edu>
2022-04-15 15:47:36 -0400

    Add preliminary main tests

---
+++ tests/test_main.py
@@ -0,0 +1,25 @@
+from typer.testing import CliRunner
+
+runner = CliRunner()
+
+
+def test_gatorgrade_runs_correct():
+    """Test that ensures that the default command runs correctly."""
+    assert True
+
+
+def test_generate_creates_correctly():
+    """Test that ensures that the generate command creates the .yml file correctly."""
+    assert True
+
+
+def test_generate_fail_with_existing_yml():
+    """Test that ensures that a second yml file isn't generated without the force command."""
+    # result = runner.invoke(app, "generate")
+    assert True
+
+
+def test_generate_force_command_creates_yml():
+    """Test that ensures the force command works correctly."""
+    # result = runner.invoke(app, "generate", "--force")
+    assert True
