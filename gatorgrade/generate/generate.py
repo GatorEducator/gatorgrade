2c530637c7681f2753ff9b77251e6df83f84bfed
Yanqiao Chen <chen02@allegheny.edu>
2022-04-15 21:55:20 -0400

    Modularize the codes
    The init file was not modulized, which
    may not be sustainable and testable
    Use functions to split codes in units

--- gatorgrade/generate/generate.py
+++ gatorgrade/generate/generate.py
@@ -15,38 +15,40 @@ ignore_files_list = [
      "__init__.py"]
 
 # Final paths to write to the gatorgrade.yml file
-final_paths = []
 
-for dirpath, dirnames, filenames in os.walk("."):
-    """Generate the file names in a directory tree by walking the three top-down"""
+def create_targeted_paths_list():
+    final_paths = []
+    for dirpath, dirnames, filenames in os.walk("."):
+        """Generate the file names in a directory tree by walking the three top-down"""
 
-    # Remove directories from directory lists based on ignore_directory_list
-    for name in ignore_directory_list:
-        if name in dirnames:
-            dirnames.remove(name)
+        # Remove directories from directory lists based on ignore_directory_list
+        for name in ignore_directory_list:
+            if name in dirnames:
+                dirnames.remove(name)
 
-    # Remove files from files lists based on ignore_files_list
-    for name in ignore_files_list:
-        if name in filenames:
-            filenames.remove(name)
+        # Remove files from files lists based on ignore_files_list
+        for name in ignore_files_list:
+            if name in filenames:
+                filenames.remove(name)
 
-    # Print path to all filenames
-    for filename in filenames:
-        # Join the directory path with the filename
-        # print(os.path.join(dirpath, filename))
-        final_paths.append(os.path.join(dirpath, filename))
+        # Print path to all filenames
+        for filename in filenames:
+            # Join the directory path with the filename
+            # print(os.path.join(dirpath, filename))
+            final_paths.append(os.path.join(dirpath, filename))
+    return final_paths
 
-    
-# Create a new YAML file 
-gatorgrader_file = open("gatorgrade.yml", "w")
+def write_yaml_of_paths_list(path_names):
+    # Create a new YAML file 
+    gatorgrader_file = open("gatorgrade.yml", "w")
 
-# Write to the file the name of the paths
-for path in final_paths:
-    without_dot = path.strip("./")
-    gatorgrader_file.write(f"{without_dot}:\n\t--description:\n\tcheck: MatchFileFragment\n\toptions:\n\t\tcount:\n\t\tfragment: TODO\n\t\texact: {True}")
-    # gatorgrader_file.write(f"{without_dot}:\n")
-    gatorgrader_file.write("\n")
-    gatorgrader_file.write("\n")
+    # Write to the file the name of the paths
+    for path in path_names:
+        without_dot = path.strip("./")
+        gatorgrader_file.write(f"{without_dot}:\n\t--description:\n\tcheck: MatchFileFragment\n\toptions:\n\t\tcount:\n\t\tfragment: TODO\n\t\texact: {True}")
+        # gatorgrader_file.write(f"{without_dot}:\n")
+        gatorgrader_file.write("\n")
+        gatorgrader_file.write("\n")
 
-# Close the file
-gatorgrader_file.close()
+    # Close the file
+    gatorgrader_file.close()
