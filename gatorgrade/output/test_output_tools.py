7d212b31e94b89ba834e236386b764c3248cee81
Garrison Vanzin <54772966+gvanzin-allegheny@users.noreply.github.com>
2022-04-19 16:29:51 -0400

    Add test for list to string in output tools

--- gatorgrade/output/test_output_tools.py
+++ gatorgrade/output/test_output_tools.py
@@ -2,3 +2,11 @@
 import pytest
 from output import output_tools
 
+def test_list_to_string_function():
+    """Test list to string function in output tools"""
+
+    list = ['test', 'test2', 'test9', 'puddle']
+    expected_string = "test test2 test3 test9 puddle"
+    actual_list = output_tools.get_simple_command_string(list)
+
+    assert expected_list == actual_list
