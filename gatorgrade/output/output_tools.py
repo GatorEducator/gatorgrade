c5f29d427ea9fa08628a6db9e59a02c367f02c88
Garrison Vanzin <54772966+gvanzin-allegheny@users.noreply.github.com>
2022-04-19 15:22:55 -0400

    Add convert to string command function

--- gatorgrade/output/output_tools.py
+++ gatorgrade/output/output_tools.py
@@ -2,10 +2,6 @@
 # The main use of this class is to de-clutter output_functions.py for better readability.
 # One example of a function to be written here is the splitting of strings retrieved from the input team, at spaces, into lists.
 
-def get_directory_and_file():
-     formatted_command.append('--file')
-                formatted_command.append(file_name)
-            if "--directory" not in formated_command:
-                formatted_command.append('--file')
-                formatted_command.append(file_name)
-            split_commands.append(fomatted_command)
+def get_simple_command_string(command):
+    command_string = ' '.join(command)
+    return command_string
