eabf039373120278d65bb52a9c0b22cc7ce753b1
Yanqiao Chen <chen02@allegheny.edu>
2022-04-19 15:47:57 -0400

    Add period for docstrings and first line docstring

--- gatorgrade/generate/generate.py
+++ gatorgrade/generate/generate.py
@@ -1,15 +1,16 @@
+"""Generate a YAML file with default messages and specific paths."""
 # Import the necessary libraries
 
 def create_targeted_paths_list():
-    """Generate a list of targeted paths by walking the paths"""
+    """Generate a list of targeted paths by walking the paths."""
     # Go through the root repo, the sub dictionaries and files.
     # Select only files in the dictionaries with specific names.
-    # Add those targeted file paths into a list and returns it
+    # Add those targeted file paths into a list and returns it.
     pass
 
 def write_yaml_of_paths_list(path_names):
-    """Write YAML file to create gatorgrade file and set default messages"""
-    # Create a new YAML file with PyYaml in the specific path
-    # write the default set up messages in YAML file
-    # List the file paths in specific format
+    """Write YAML file to create gatorgrade file and set default messages."""
+    # Create a new YAML file with PyYaml in the specific path.
+    # write the default set up messages in YAML file.
+    # List the file paths in specific format.
     pass
