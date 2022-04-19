4335e3c19936154449927bc5521c70cfab912273
Yanqiao Chen <chen02@allegheny.edu>
2022-04-19 18:02:29 -0400

    Pass lint check

--- gatorgrade/generate/generate.py
+++ gatorgrade/generate/generate.py
@@ -1,16 +1,21 @@
-"""Generate a GatorGrade configuration file with paths to whitelisted files and default GatorGrader checks"""
+"""Generate a GatorGrade configuration file.
+
+Gatorgrade file will include paths to whitelisted files
+and default GatorGrader checks.
+"""
+
 # Import the necessary libraries
+
 
 def create_targeted_paths_list():
     """Generate a list of targeted paths by walking the paths."""
     # Go through the root repo, the sub dictionaries and files.
     # Select only files in the dictionaries with specific names.
     # Add those targeted file paths into a list and returns it.
-    pass
 
-def write_yaml_of_paths_list(path_names):
+
+def write_yaml_of_paths_list():
     """Write YAML file to create gatorgrade file and set default messages."""
     # Create a new YAML file with PyYaml in the specific path.
     # Write the default set up messages in YAML file.
     # List the file paths in specific format.
-    pass
