760a5a56f6c4a5250cc9888126ae1fe73c55df98
Liam Black <blackl@allegheny.edu>
2022-04-19 14:59:34 -0400

    documentation for file path

---
+++ gatorgrade/input/in_file_path.py
@@ -0,0 +1,18 @@
+"""Allow customer to specify gator grader checks that do and do not correspond to a file path."""
+
+# import yaml and other necessary libraries/packages
+
+def generate_checks(# list of tuples):
+# iterate through the checks
+
+# if check corresponds to file path print the string
+
+# description = data['description']
+#       options = data['options']
+#       arguments = ['--description', f'{description}']
+#       arguments.append(data['check'])
+#       for key, value in options.items():
+#         arguments.append(f'--{key}')
+#         arguments.append(f'{value}')
+
+# else check does not correspond to file path print the string needed
