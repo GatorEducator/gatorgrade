48d0613095e7810d140c16d36310bb24d2eae253
tuduun <gantulga01@allegheny.edu>
2022-04-19 16:29:39 -0400

    Add generate checks implementation

--- gatorgrade/input/in_file_path.py
+++ gatorgrade/input/in_file_path.py
@@ -2,8 +2,23 @@
 
 # import yaml and other necessary libraries/packages
 
-def generate_checks(# list of tuples):
+
+def generate_checks(tuple):
 # iterate through the checks
+    list_  = tuple[1]
+    file_path = tuple[0]
+
+    if tuple[0] is not None:
+        for dicts in list_data:
+            for keys in dicts:
+                description = dicts['description']
+                options = dicts['options']
+                #creating a list that has description, check, and options in it.
+                gator_grader_commands = ['--description', f'{description}']
+                gator_grader_commands.append(data['check'])
+
+
+
 
 # if check corresponds to file path print the string
 
@@ -16,3 +31,7 @@ def generate_checks(# list of tuples):
 #         arguments.append(f'{value}')
 
 # else check does not correspond to file path print the string needed
+
+
+# for options:arguments.append(f'--{key}')
+#arguments.append(f'{dicts[key]}')
