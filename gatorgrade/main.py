b88d2ec454aa59786d00bb3be004649e4a799eed
burgess01 <burgess01@allegheny.edu>
2022-04-19 15:32:17 -0400

    Add yml file location option

--- gatorgrade/main.py
+++ gatorgrade/main.py
@@ -1,11 +1,13 @@
 import typer
+from pathlib import Path
 
 app = typer.Typer(add_completion=False)
+FILE_DIR = "config"
 FILE = "gatorgrade.yml"
 
 
 @app.callback(invoke_without_command=True)
-def gatorgrade():
+def gatorgrade(file_dir: Path = typer.Option(f"{FILE_DIR}/{FILE}")):
     """Runs the GatorGrader checks that are defined in the gatorgrade.yml file."""
 
 
