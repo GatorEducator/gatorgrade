39a318d1ab7f32483e6bb117fab62f5156ee50d0
Yanqiao Chen <chen02@allegheny.edu>
2022-04-19 16:17:12 -0400

    Add implementation

--- gatorgrade/generate/generate.py
+++ gatorgrade/generate/generate.py
@@ -1,11 +1,24 @@
 """Generate a YAML file with default messages and specific paths."""
+import os
 # Import the necessary libraries
 
 def create_targeted_paths_list():
     """Generate a list of targeted paths by walking the paths."""
+    targeted_paths = []
     # Go through the root repo, the sub dictionaries and files.
-    # Select only files in the dictionaries with specific names.
-    # Add those targeted file paths into a list and returns it.
+    for dirpath, _, filenames in os.walk("."):
+
+        # Select only files in the dictionaries with specific names.
+        for filename in filenames:
+            path_list = dirpath.split("/")
+            # Check the key words only in the first and the second levels of paths
+            if "writing" or "src" or "test" in path_list[1] or path_list[2]:
+
+            # Add those qualified file paths into a list and returns it.
+                targeted_paths.append(os.path.join(dirpath, filename))
+
+
+
     pass
 
 def write_yaml_of_paths_list(path_names):
