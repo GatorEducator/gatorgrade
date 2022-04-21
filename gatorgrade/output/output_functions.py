41fa65ce36f1b081237f96ab8734cdfa963bc35b
Luke Lacaria <lacaria01@allegheny.edu>
2022-04-21 17:52:20 -0400

    Add beginning of output check functions

--- gatorgrade/output/output_functions.py
+++ gatorgrade/output/output_functions.py
@@ -8,7 +8,7 @@ Won't work until Poetry is in the project
 '''
 def sample_output_passing_check():
     req = "REQUIREMENT" # Description of test requirement
-    loc = "LOCATION" # File location
+    loc = "" # File location
     num = "NUMBER" # Number of elements required
     print(Style.RESET_ALL)
     if len(loc) > 0:
@@ -30,8 +30,38 @@ def sample_output_failing_check():
     else:
         print(Fore.RED + "\u2718 " + req)
     
+'''
+Process results and determine if the check passed or failed.
+
+Argument `result` is: list[FILENAME, [(check_result_element1, check_result_element2),(...)]]
+'''
+def determine_result_by_file(result):
+    file_name = result[0]
+    for i in result[1]:
+        output_check_result(file_name, i)
+
+'''
+Produce the output for the console for each check.
+
+Argument: take check from determine_result_by_file()
+as a tuple in the (REQUIREMENT, PASS/FAIL, RESULT-optional) format.
+'''
+def output_check_result(file, check):
+    # Store the check elements as variables for the output statement.
+    requirement = check[1]
+    result = check[2]
+    # Print message with green check for passing checks
+    if result == True:
+        print(f"{Fore.GREEN}\u2714  {Style.RESET_ALL}{file} {requirement}")
+    # Print message with X for failing checks
+    elif result == False:
+        print(f"{Fore.RED}\u2718  {Style.RESET_ALL}{file} {requirement}")
+
+            
+
+    
 
 
 # Print outputs
-sample_output_passing_check()
-sample_output_failing_check()
+sample_result = ["file.txt", [('No TODOS in text', True), ('Has an if statement', False, "No if statements found")]]
+determine_result_by_file(sample_result)
