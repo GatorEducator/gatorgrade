c602d3b613177a70d854feee7510f89ba2f44127
Yanqiao Chen <chen02@allegheny.edu>
2022-04-22 16:10:56 -0400

    Ignore hidden folders

--- gatorgrade/generate/generate.py
+++ gatorgrade/generate/generate.py
@@ -1,22 +1,30 @@
 """Generate a YAML file with default messages and specific paths."""
+from importlib.resources import path
 import os
 # Import the necessary libraries
 
+
+# First, iterate the paths
+# Only target the paths where the key word shows up in the first or second level
+# Combine paths with files to create the complete paths
+# Put paths into a list
 
 def create_targeted_paths_list():
     """Generate a list of targeted paths by walking the paths."""
     targeted_paths = []
     key_word_list = ["writing", "src", "test"]
     # Go through the root repo, the sub dictionaries and files.
+    # The os.walk will only scan the paths. So the empty folders containing nothing won't be gone through.
     for dirpath, _, filenames in os.walk("."):
         # Select only files in the dictionaries with specific names.
         for filename in filenames:
             path_list = dirpath.split("/")
-
-            # Add paths only when they have the key words in the first and the second levels
-            # The first path will always be dot, so skip it.
             if len(path_list) < 2:
                 continue
+            if path_list[1].startswith("."):
+                continue
+            # Add paths only when they have the key words in the first and the second levels
+            # The first path will always be dot, so skip it.
             elif len(path_list) == 2:
                 for key in key_word_list:
                     if key in path_list[1]:
@@ -34,3 +42,5 @@ def write_yaml_of_paths_list(path_names):
     # write the default set up messages in YAML file.
     # List the file paths in specific format.
     pass
+
+print(create_targeted_paths_list())
