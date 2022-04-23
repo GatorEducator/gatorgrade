f0c7b59e8bbfb1d0accc38fe8e205a35fdadc68c
burgess01 <burgess01@allegheny.edu>
2022-04-23 17:02:24 -0400

    included function calls in tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,26 +1,31 @@
 from typer.testing import CliRunner
 
+from gatorgrade import main
+
 runner = CliRunner()
 
 
 def test_gatorgrade_runs():
     """Test that ensures that the default command runs correctly."""
+    result = runner.invoke(main.app)
     assert True
 
 
 def test_generate_creates_valid_yml():
     """Test that ensures that the generate command creates the .yml file correctly."""
-    # use pytest-mock to test if function with force is called correctly
+    result = runner.invoke(main.app, "--force")
     assert True
 
 
 def test_generate_fails_with_existing_yml():
     """Test that ensures that a second yml file isn't generated without the force command."""
-    # result = runner.invoke(app, "generate")
+    result = runner.invoke(main.app)
+
     assert True
 
 
 def test_generate_force_option_creates_yml():
     """Test that ensures the force command works correctly."""
-    # result = runner.invoke(app, "generate", "--force")
+    result = runner.invoke(main.app, "--force")
+
     assert True
