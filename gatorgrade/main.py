0f69cf482f4bf5777dafee56cd578a00b032131e
burgess01 <burgess01@allegheny.edu>
2022-04-23 17:33:57 -0400

    Add example code for linting

--- gatorgrade/main.py
+++ gatorgrade/main.py
@@ -8,12 +8,15 @@ FILE = "gatorgrade.yml"
 
 @app.callback(invoke_without_command=True)
 def gatorgrade(
-    filepath: Path = typer.Option(
+    filename: Path = typer.Option(
         f"{FILE}", "--config", "-c", help="Name of the yml file."
     )
 ):
     """Runs the GatorGrader checks that are defined
     in the gatorgrade.yml file."""
+
+    if filename.contains(".yml"):
+        pass
 
 
 @app.command()
@@ -27,6 +30,8 @@ def generate(
 ):
     """Generates a gatorgrade.yml file based on the folders and files
     in the current directory."""
+    if force is False:
+        pass
 
 
 if __name__ == "__main__":
