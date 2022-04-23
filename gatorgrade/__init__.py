fffb1a0f40d47181bb448bb6e9ac4c1b4bd96347
burgess01 <burgess01@allegheny.edu>
2022-04-23 17:14:24 -0400

    Restructure docstring for character limit

--- gatorgrade/__init__.py
+++ gatorgrade/__init__.py
@@ -1,1 +1,2 @@
-"""GatorGrade is a program that checks your work fulfillments according to grade check accommodations."""
+"""GatorGrade is a program that checks your work fulfillments
+according to grade check accommodations."""
