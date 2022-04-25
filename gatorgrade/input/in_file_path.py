bd4ba1dc15541fb8c4d1b6a3bc630852b051e742
Jacob-Allebach <allebach01@allegheny.edu>
2022-04-25 11:22:26 -0400

    Add docstring and reduce code

--- gatorgrade/input/in_file_path.py
+++ gatorgrade/input/in_file_path.py
@@ -1,10 +1,7 @@
 import yaml
 
-#Parses a YAML file and returns its contents as a list of dictionaries
 def parse_yaml_file(file_path):
+    """Parses a YAML file and returns its contents as a list of dictionaries"""
     with open(file_path) as f:
         data = yaml.load_all(f, Loader=yaml.FullLoader)
-        data_holder = []
-        for item in data: #Transfers the data from the file's generator to a list for later access
-            data_holder.append(item)
-        return data_holder
+        return list(data)
