0f0f0d088e0e452fb9154e1ada5e2af9b1eaf267
Liam Black <blackl@allegheny.edu>
2022-04-25 11:52:24 -0400

    Add documentaion to check-in-filetest

--- tests/input/test_input.py
+++ tests/input/test_input.py
@@ -1,6 +1,22 @@
+"""Test suite for parse_config function."""
+
 from gatorgrade.input.in_file_path import parse_config
 
 def test_parse_config_gg_check_in_file_context_contains_file():
+    # Given the path to the test yml file
     config = "tests/input/gatorgrade_one_gg_check_in_file_context.yml"
+    # when the parse_config function is run
     output = parse_config(config)
+    # then assert that the file path exists in the first index of the list
     assert "file.py" in output[0]  
+
+def test_parse_config_gg_check_no_file_context_contains_no_file():
+    # Given the path to the test yml file
+
+    # When parse_config is run
+
+
+    # Then assert that there is no file path indicated in the first index of list
+
+
+
