d73741acdfda7bc9afae4ebd38a2f43dc5ce2d35
tuduun <gantulga01@allegheny.edu>
2022-04-25 11:00:27 -0400

    Add the text file

--- gatorgrade/input/in_file_path.py
+++ gatorgrade/input/in_file_path.py
@@ -1,30 +1,33 @@
 """Allow customer to specify gator grader checks that do and do not correspond to a file path."""
 
 # import yaml and other necessary libraries/packages
-
+txt = [{'description': 'Complete all TODOs', 'check': 'MatchFileFragment', 'options': {'fragment': 'TODO', 'count': 0, 'exact': True}}, {'description': 'Use an if statement', 'check': 'MatchFileRegex', 'options': {'regex': 'if .*?:', 'count': 1, 'exact': False}}]
 
 def generate_checks(tuple_of_file):
 # iterate through the checks
-    list_of_dict_checks  = tuple[1]
-    file_path = tuple[0]
+    list_of_dict_checks  = tuple_of_file
+    #file_path = tuple_of_file[0]
 
-    if file_path is not None:
-        for dicts in list_data:
-            for keys in dicts:
-                description = dicts['description']
-                options = dicts['options']
-                #creating a list that has description, check, and options in it.
-                gator_grader_commands = ['--description', f'{description}']
-                gator_grader_commands.append(dicts['check'])
-                #if options exist, then add all the keys and the values inside the command list
-                if options:
-                    for keys in options:
-                        gator_grader_commands.append(f'--{keys}')
-                        gator_grader_commands.append(f'{options[keys]}')
+    #if file_path is not None:
+    for dicts in list_of_dict_checks:
+        for keys in dicts:
+            description = dicts['description']
+            options = dicts['options']
+            #creating a list that has description, check, and options in it.
+            gator_grader_commands = ['--description', f'{description}']
+            gator_grader_commands.append(dicts['check'])
+            #if options exist, then add all the keys and the values inside the command list
+            if options:
+                for keys in options:
+                    gator_grader_commands.append(f'--{keys}')
+                    gator_grader_commands.append(f'{options[keys]}')
+    return gator_grader_commands
 
 
 
+checks = generate_checks(txt)
 
+print(checks)
 
 
 
