526da15c6dff971a8f9ed29ef647a40358ae1509
Peyton Kelly <pkelly5511@gmail.com>
2022-04-25 10:41:33 -0400

    Importing 'output_functions'  to test

--- gatorgrade/output/tests/test_function.py
+++ gatorgrade/output/tests/test_function.py
@@ -1,12 +1,13 @@
 from sys import stdout
-from output_functions import output_fail_description, sample_output_passing_check
+import output_functions
+
 import pytest
-from colorama import init, Fore, Back, Style
+from colorama import init, Fore, Style
 
 
 @pytest.fixture()
 def test_output_shows_green(capsys):
-    sample_output_passing_check()
+    output_functions.sample_output_passing_check()
     out, err = capsys.readouterr()
 
     assert f"{Fore.GREEN}\u2714" in out
@@ -14,35 +15,42 @@ def test_output_shows_green(capsys):
 
 
 def test_output_shows_red(capsys):
-    sample_output_passing_check()
+    output_functions.sample_output_passing_check()
     out, err = capsys.readouterr()
 
     assert f"{Fore.RED}\u2718" in out
-
+    assert err == ""
 
 
 def test_output_shows_yellow(capsys):
 
-    sample_output_passing_check()
+    output_functions.sample_output_passing_check()
     out, err = capsys.readouterr()
 
     assert f"{Fore.YELLOW}\u2192" in out
-
+    assert err ==  ""
 
 
 def test_descrition_in_fail_message(capsys):
-    output_fail_description()
+    output_functions.output_fail_description()
     out, err = capsys.readouterr()
 
     assert "No if statements found" in out 
+    assert err == ""
+
+
+def test_false_result_returns_X():
+    output_functions.output_check_result()
+
+
 
 '''
 def test_result():
 
 
 
-Def test_false_result_returns_X
 
+    
 
 Def test_passing_result_has_one_line 
 
@@ -52,4 +60,7 @@ def test_output_will_show_diagnostic
 
 def test_Description_taken_as_argument
 
+
+
+def test style reset changes color
 '''
