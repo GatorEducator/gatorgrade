3364b6172294215c78fb9ec77fe422f1cc3a544c
Jacob-Allebach <allebach01@allegheny.edu>
2022-04-26 16:11:20 -0400

    Fix some more linting errors

--- gatorgrade/input/in_file_path.py
+++ gatorgrade/input/in_file_path.py
@@ -3,6 +3,6 @@ import yaml
 
 def parse_yaml_file(file_path):
     """Parses a YAML file and returns its contents as a list of dictionaries"""
-    with open(file_path) as f:
-        data = yaml.load_all(f, Loader=yaml.FullLoader)
+    with open(file_path, encoding="utf8") as file:
+        data = yaml.load_all(file, Loader=yaml.FullLoader)
         return list(data)
