8a819ce7dfd62f80e8e50c8b5ce2380c70bf42af
Liam Black <blackl@allegheny.edu>
2022-04-26 15:59:15 -0400

    Fix deletion error

--- tests/input/test_input_gg_checks.py
+++ tests/input/test_input_gg_checks.py
@@ -19,4 +19,20 @@ def test_parse_config_gg_check_no_file_context_contains_no_file():
     # Then assert that there is no file path indicated in the first index of list
     assert output[0] == ["--description", "Have 8 commits", "CountCommits", "--count", "8"]
 
+def test_parse_config_gg_check_no_file_context_contains_no_file_path():
+    # Given the path to the test yml file
+    config = "tests/input/gatorgrade_one_gg_check_no_file_context.yml"
+    # When parse_config is run
+    output = parse_config(config)
 
+    # Then assert that there is no file path indicated in the first index of list
+    assert "file.py" not in output[0][0]
+
+
+def test_parse_config_gg_check_in_file_context_contains_file():
+    # Given the path to the test yml file
+    config = parse_config(config)
+
+    # When parse_config is run
+
+    # Then assert
