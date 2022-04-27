e33d0b78b7636643879e89baba9225975c24563f
Yanqiao Chen <chen02@allegheny.edu>
2022-04-27 17:25:50 -0400

    Ignore double underscore start
    For the files and folders starting with underscore, ignore
     them because they don't follow the naming convention

--- gatorgrade/generate/generate.py
+++ gatorgrade/generate/generate.py
@@ -1,39 +1,39 @@
 """Generate a YAML file with default messages and specific paths."""
-from importlib.resources import path
 import os
-# Import the necessary libraries
-
-
-# First, iterate the paths
-# Only target the paths where the key word shows up in the first or second level
-# Combine paths with files to create the complete paths
-# Put paths into a list
 
 def create_targeted_paths_list():
     """Generate a list of targeted paths by walking the paths."""
     targeted_paths = []
-    key_word_list = ["writing", "src", "test"]
+    key_word_list = ["writing", "src", "test", "deliverables"]
     # Go through the root repo, the sub dictionaries and files.
-    # The os.walk will only scan the paths. So the empty folders containing nothing won't be gone through.
+    # The os.walk will only scan the paths. So the empty folders containing nothing won't be gone through
     for dirpath, _, filenames in os.walk("."):
-        for filename in filenames:
-            # Split path string to split it into multiple directories.
-            path_dir_list = dirpath.split("/")
-            # The first directory is always dot, so skip it.
-            if len(path_dir_list) == 1:
-                continue
-            # Ignore hidden folders.
-            if path_dir_list[1].startswith("."):
+
+        # Split path string into multiple layers of directories
+        path_dir_list = dirpath.split("/")
+        # Ignore folder starting with __
+        for path in path_dir_list:
+            if path.startswith("__"):
                 continue
 
-            # Add paths only when they have the key words in the second and the third directories.
+        for filename in filenames:
+            # The first directory is always dot. Skip it
+            if len(path_dir_list) == 1:
+                continue
+            # Ignore the file starting with __
+            if filename.startswith("__"):
+                continue
+            # Ignore hidden folders
+            if path_dir_list[1].startswith("."):
+                continue
+            # Add paths only when they have the key words in the second and the third directories
             elif len(path_dir_list) == 2:
                 for key in key_word_list:
-                    # For the path with only two directories, check key words in the second directory folder name.
+                    # For the path with only two directories, check key words in the second directory folder name
                     if key in path_dir_list[1]:
                         targeted_paths.append(os.path.join(dirpath, filename))
 
-            # For the other paths with more than 2 directories, check key words in the second and third directories.
+            # For the other paths with more than 2 directories, check key words in the second and third directories
             else:
                 for key in key_word_list:
                     if key in path_dir_list[1] or key in path_dir_list[2]:
