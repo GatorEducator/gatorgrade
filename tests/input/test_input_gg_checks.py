1d468bca38b3910fe17b7243996f7561712281c3
Liam Black <blackl@allegheny.edu>
2022-04-28 13:05:10 -0400

    Remove duplicate behavior test

--- tests/input/test_input_gg_checks.py
+++ tests/input/test_input_gg_checks.py
@@ -8,7 +8,7 @@ def test_parse_config_gg_check_in_file_context_contains_file():
     # when the parse_config function is run
     output = parse_config(config)
     # then assert that the file path exists in the first index of the list
-    assert "file.py" in output[0]  
+    assert "file.py" in output[0][0]  
 
 def test_parse_config_gg_check_no_file_context_contains_no_file():
     # Given the path to the test yml file
@@ -19,14 +19,7 @@ def test_parse_config_gg_check_no_file_context_contains_no_file():
     # Then assert that there is no file path indicated in the first index of list
     assert output[0] == ["--description", "Have 8 commits", "CountCommits", "--count", "8"]
 
-def test_parse_config_gg_check_no_file_context_contains_no_file_path():
-    # Given the path to the test yml file
-    config = "tests/input/gatorgrade_one_gg_check_no_file_context.yml"
-    # When parse_config is run
-    output = parse_config(config)
 
-    # Then assert that there is no file path indicated in the first index of list
-    assert "file.py" not in output[0][0]
 
 
 def test_parse_config_gg_check_in_file_context_contains_file():
