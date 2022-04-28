5554b69ded48dd602e60f97176c7281aee7448e9
tuduun <gantulga01@allegheny.edu>
2022-04-27 21:00:37 -0400

    Add os lib, returns file directory and file name

--- gatorgrade/input/in_file_path.py
+++ gatorgrade/input/in_file_path.py
@@ -1,20 +1,15 @@
 """Allow customer to specify gator grader checks that do and do not correspond to a file path."""
 
+# import necessary libraries/packages
 import os
-# import yaml and other necessary libraries/packages
-txt = ("the_file_path", [{'description': 'Complete all TODOs', 'check': 'MatchFileFragment', 'options': {'fragment': 'TODO', 'count': 0, 'exact': True}}, {'description': 'Use an if statement', 'check': 'MatchFileRegex', 'options': {'regex': 'if .*?:', 'count': 1, 'exact': False}}])
 
-shell = (None, [{'description': 'Pass HTMLHint', 'command': 'htmlhint'},{'description': 'Pass HTMLHint', 'command': 'htmlhint'}])
-
-non_file = (None, [{'description': 'Have a total of 8 commits, 5 of which were created by you', 'check': 'CountCommitts', 'options': {'fragment': 'TODO', 'count': 0, 'exact': True}}, {'description': 'Use an if statement', 'check': 'MatchFileRegex', 'options': {'regex': 'if .*?:', 'count': 1, 'exact': False}}])
-
-a_list_of_tuples = [("the_file_path", [{'description': 'Complete all TODOs', 'check': 'MatchFileFragment', 'options': {'fragment': 'TODO', 'count': 0, 'exact': True}}, {'description': 'Use an if statement', 'check': 'MatchFileRegex', 'options': {'regex': 'if .*?:', 'count': 1, 'exact': False}}]),
+#test list of tuples that has checks
+a_list_of_tuples = [("./home/dir/subdir/yayaya.py",[{'description': 'Complete all TODOs', 'check': 'MatchFileFragment', 'options': {'fragment': 'TODO', 'count': 0, 'exact': True}}, {'description': 'Use an if statement', 'check': 'MatchFileRegex', 'options': {'regex': 'if .*?:', 'count': 1, 'exact': False}}]),
+                ("./home/dir/subdir/module.py", [{'description': 'Complete all TODOs', 'check': 'MatchFileFragment', 'options': {'fragment': 'TODO', 'count': 0, 'exact': True}}, {'description': 'Use an if statement', 'check': 'MatchFileRegex', 'options': {'regex': 'if .*?:', 'count': 1, 'exact': False}}]),
                   (None, [{'description': 'Pass HTMLHint', 'command': 'htmlhint'},{'description': 'Have a total of 8 commits, 5 of which were created by you', 'check': 'CountCommitts', 'options': {'fragment': 'TODO', 'count': 0, 'exact': True}}])]
 
-new_tuple = (None, [{'description': 'Pass HTMLHint', 'command': 'htmlhint'},{'description': 'Have a total of 8 commits, 5 of which were created by you', 'check': 'CountCommitts', 'options': {'fragment': 'TODO', 'count': 0, 'exact': True}}, {'description': 'Use an if statement', 'check': 'MatchFileRegex', 'options': {'regex': 'if .*?:', 'count': 1, 'exact': False}}])
 
-
-#function for working on a list of tuples
+# Function to generate command lines from a list of tuples
 
 def command_line_generator_list(a_list_of_tuples):
     """Generate a list of gator grader and shell command lines using the parsed data."""
@@ -29,8 +24,8 @@ def command_line_generator_list(a_list_of_tuples):
             # We need description to be declared differently because every check has it
             # If the check has a 'command', then it is a shell check
             if 'command' in parsed_dict_check:
+                # Add the shell checks
                 shell_checks.append(parsed_dict_check)
-                # Add the temporary shell check raw dictionary into the final shell check list
             else:
                 description = parsed_dict_check['description']
                 temp_gator_grader_commands = []
@@ -51,87 +46,17 @@ def command_line_generator_list(a_list_of_tuples):
                 # If it is a gator grade check with a file, then add the directory and the file name
                 if file_path is not None:
                     # Get the file directory using os
-                    cwd = os.getcwd()
+                    dirname, filename = os.path.split(file_path)
                     temp_gator_grader_commands.append(f'--directory')
-                    temp_gator_grader_commands.append(f'{cwd}')
+                    temp_gator_grader_commands.append(f'{dirname}')
                     temp_gator_grader_commands.append(f'--file')
-                    temp_gator_grader_commands.append(f'{file_path}')
+                    temp_gator_grader_commands.append(f'{filename}')
                 # Add the contents inside the temporary list into the final gator grader list.
                 gator_grader_checks.append(temp_gator_grader_commands)
 
     return {"shell": shell_checks,"gatorgrader": gator_grader_checks}
 
 
-#final_gator_grader_commands, final_shell_commands = command_line_generator(shell)
-#print(final_gator_grader_commands, final_shell_commands)
+dict_of_shell_and_gator_commands = command_line_generator_list(a_list_of_tuples)
+print(dict_of_shell_and_gator_commands)
 
-def command_line_generator(tuple_check):
-    """Generate a list of gator grader and shell command lines using the parsed data."""
-    file_path = tuple_check[0]
-    list_of_dict_checks  = tuple_check[1]
-    #Declaring final output for the checks
-    gator_grader_checks = []
-    shell_checks = []
-    for parsed_dict_check in list_of_dict_checks:
-        # Creating temporary lists for every check in the dictionary
-        # We need description to be declared differently because every check has it
-        description = parsed_dict_check['description']
-        # If the check has a 'command', then it is a shell check
-        if 'command' in parsed_dict_check:
-            shell_checks.append(parsed_dict_check)
-            # Add the temporary shell check raw dictionary into the final shell check list
-        else:
-            temp_gator_grader_commands = []
-            options = parsed_dict_check['options']
-            # Creating a temp list that has description, check, and options in it for every check.
-            temp_gator_grader_commands = ['--description', f'{description}']
-            temp_gator_grader_commands.append(parsed_dict_check['check'])
-            # If options exist, then add all the keys and the values inside the temp command list
-            if options:
-                for key in options:
-                    # if the type of the value is boolean, only add the key not the boolean value
-                    if type(options[key]) == bool:
-                        if options[key] == True:
-                            temp_gator_grader_commands.append(f'--{key}')
-                    else:
-                        temp_gator_grader_commands.append(f'--{key}')
-                        temp_gator_grader_commands.append(f'{options[key]}')
-            # If it is a gator grade check with a file, then add the directory and the file name
-            if file_path is not None:
-                dirname, basename = os.path.split(filepath)
-                # Get the file directory using os
-                cwd = os.getcwd()
-                temp_gator_grader_commands.append(f'--directory')
-                temp_gator_grader_commands.append(f'{cwd}')
-                temp_gator_grader_commands.append(f'--file')
-                temp_gator_grader_commands.append(f'{file_path}')
-            # Add the contents inside the temporary list into the final gator grader list.
-            gator_grader_checks.append(temp_gator_grader_commands)
-
-    return {"shell": shell_checks,"gatorgrader": gator_grader_checks}
-
-'''
-    #If gator grader check is not empty, then we will return it.
-    if gator_grader_checks:
-        return gator_grader_checks
-    #If the gator grader check is empty, then we will return the shell commands.
-    else:
-        return shell_checks
-'''
-
-
-#command_line =  command_line_generator(new_tuple)
-#print(command_line)
-
-gg= command_line_generator_list(a_list_of_tuples)
-print(gg)
-
-
-
-
-
-
-
-
-#final_gator_grader_commands, final_shell_commands = command_line_generator(shell)
-#print(final_gator_grader_commands, final_shell_commands)
