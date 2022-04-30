d5298643324a79295fd76dab42d3d6cd9a8b9ddf
Jacob-Allebach <allebach01@allegheny.edu>
2022-04-30 19:40:20 -0400

    Fix linting error

--- gatorgrade/input/in_file_path.py
+++ gatorgrade/input/in_file_path.py
@@ -8,14 +8,16 @@ def parse_yaml_file(file_path):
         data = yaml.load_all(file, Loader=yaml.FullLoader)
         return list(data)
 
+
 def reformat_yaml_data(data):
     """Reformat the raw data from a YAML file into a list of tuples."""
     reformatted_data = []
-    data.pop(0) #Removes the setup commands
-    loop_through_data(None,data[0],reformatted_data)
+    data.pop(0)  # Removes the setup commands
+    loop_through_data(None, data[0], reformatted_data)
     return reformatted_data
 
-def loop_through_data(path,data_list,reformatted_data):
+
+def loop_through_data(path, data_list, reformatted_data):
     """Recursively loop through the data and add any checks that are found to the reformatted list."""
     current_path = path
     for dict in data_list:
@@ -25,8 +27,8 @@ def loop_through_data(path,data_list,reformatted_data):
                     path = item
                 else:
                     path = f"{path}/{item}"
-                loop_through_data(path,dict[item],reformatted_data)
+                loop_through_data(path, dict[item], reformatted_data)
                 path = current_path
             else:
-                reformatted_data.append({"file_context":path,"check":dict})
+                reformatted_data.append({"file_context": path, "check": dict})
                 break
