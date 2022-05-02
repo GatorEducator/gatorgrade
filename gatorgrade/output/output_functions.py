156678d90f57169c79e9423dc3999bcc0b8779ad
gvanzin-allegheny <vanzing@allegheny.edu>
2022-05-01 23:12:47 -0400

    Run black to format code

--- gatorgrade/output/output_functions.py
+++ gatorgrade/output/output_functions.py
@@ -7,18 +7,19 @@ import output_tools
 from typing import List, Tuple
 from gator import exceptions
 
+
 def run_gatorgrade_commands_and_return_results(commands_input):
     """Main command function to pass commands and send results to other methods.
     Commands are received as dictionary of two keys, shell commands / gator commands.
-    
-    An Example of input for this function is as follows : 
-    
-    {'shell': [{'description': 'Run program', 'command': 'mdl'}], 
-    'gatorgrader': [['--description', 'do command', 'commandType', 
+
+    An Example of input for this function is as follows :
+
+    {'shell': [{'description': 'Run program', 'command': 'mdl'}],
+    'gatorgrader': [['--description', 'do command', 'commandType',
     '--arg', '1', '--directory', './home', '--file', 'file.py']]}"""
 
     # Get first element in list, which is gatorgrader commands
-    gatorcommands = commands_input.get('gatorgrader')
+    gatorcommands = commands_input.get("gatorgrader")
     results = []
 
     for command in gatorcommands:
@@ -26,7 +27,11 @@ def run_gatorgrade_commands_and_return_results(commands_input):
         try:
             result = gator.grader(command)
             results.append(result)
-        except (exceptions.InvalidCheckArgumentsError, exceptions.InvalidSystemArgumentsError, exceptions.InvalidCheckError) as e:
+        except (
+            exceptions.InvalidCheckArgumentsError,
+            exceptions.InvalidSystemArgumentsError,
+            exceptions.InvalidCheckError,
+        ) as e:
             print(
                 "\033[91m \033[1m \033[4m An exception was detected when running the command : \033[0m \n\n \033[91m",
                 " ".join(command),
