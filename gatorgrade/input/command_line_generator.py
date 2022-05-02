cdf20734dde251476cc72ec11409f3f811bf0a81
tuduun <gantulga01@allegheny.edu>
2022-05-02 11:36:05 -0400

    run black

--- gatorgrade/input/command_line_generator.py
+++ gatorgrade/input/command_line_generator.py
@@ -6,8 +6,9 @@ import os
 
 # Function to generate command options from a list of object checks
 
+
 def generate_checks(file_context_checks):
-    """Generate a dictionary of checks based on the configuration file. 
+    """Generate a dictionary of checks based on the configuration file.
 
         This dictionary will have the format:
         {
@@ -16,7 +17,7 @@ def generate_checks(file_context_checks):
         }
 
     Args:
-        file_context_checks: List containing dictionaries that contain file contexts (either a file path or None if no file context) 
+        file_context_checks: List containing dictionaries that contain file contexts (either a file path or None if no file context)
             and checks in another dictionary (which can be any combination of GatorGrader or shell checks). The input list is generated based on the configuration file.
 
     """
@@ -24,19 +25,19 @@ def generate_checks(file_context_checks):
     shell_checks = []
     for file_context_check in file_context_checks:
         # assigning the check from the dict object
-        check = file_context_check['check']
+        check = file_context_check["check"]
         # If the check has a 'command', then it is a shell check
-        if 'command' in check:
+        if "command" in check:
             shell_checks.append(check)
-        # Else it's a gator grader check 
-        else: 
+        # Else it's a gator grader check
+        else:
             gatorgrader_command_options = []
             # Defining the description and option
-            description = check['description']
-            options = check['options']
+            description = check["description"]
+            options = check["options"]
             # Creating a list that has description, check, and options in it for every GatorGrader check.
-            gatorgrader_command_options = ['--description', f'{description}']
-            gatorgrader_command_options.append(check['check'])
+            gatorgrader_command_options = ["--description", f"{description}"]
+            gatorgrader_command_options.append(check["check"])
             # If options exist, then add all the keys and the values inside the GatorGrader command options
             if options:
                 for key in options:
@@ -44,22 +45,22 @@ def generate_checks(file_context_checks):
                     # Checking if the key is a flag
                     if type(options[key]) == bool:
                         if options[key] == True:
-                            gatorgrader_command_options.append(f'--{key}')
+                            gatorgrader_command_options.append(f"--{key}")
                     # Else if it's not a flag, then adding both key and values
                     else:
-                        gatorgrader_command_options.append(f'--{key}')
-                        gatorgrader_command_options.append(f'{options[key]}')
+                        gatorgrader_command_options.append(f"--{key}")
+                        gatorgrader_command_options.append(f"{options[key]}")
             # assigning the file context from the dict object
-            file_context = file_context_check['file_context']
+            file_context = file_context_check["file_context"]
             # If it is a gator grade check with a file context, then add the directory and the file name into the command options
             if file_context is not None:
                 # Get the file and directory using os
                 dirname, filename = os.path.split(file_context)
-                gatorgrader_command_options.append(f'--directory')
-                gatorgrader_command_options.append(f'{dirname}')
-                gatorgrader_command_options.append(f'--file')
-                gatorgrader_command_options.append(f'{filename}')
+                gatorgrader_command_options.append(f"--directory")
+                gatorgrader_command_options.append(f"{dirname}")
+                gatorgrader_command_options.append(f"--file")
+                gatorgrader_command_options.append(f"{filename}")
             # Add the contents inside the temporary list into the final GatorGrader list.
             gatorgrader_checks.append(gatorgrader_command_options)
 
-    return {"shell": shell_checks,"gatorgrader": gatorgrader_checks}
+    return {"shell": shell_checks, "gatorgrader": gatorgrader_checks}
--- gatorgrade/input/test_in_file_path.py
+++ gatorgrade/input/test_in_file_path.py
@@ -3,19 +3,14 @@
 import pytest
 from input import in_file_path
 
+
 def test_generate_checks_with_file_paths():
 
-    #Liam will write a test function here
+    # Liam will write a test function here
     # Given check commands are generated within a file path
-
 
     # when generate_checks is run
 
-
     # Then file path should be included in the list of strings
-    
 
-
-    assert  x
-
-
+    assert x
