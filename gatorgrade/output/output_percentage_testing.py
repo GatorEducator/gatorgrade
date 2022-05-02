6750eb66be2975b26bb0a16c4ef28ee66f437662
Peyton Kelly <pkelly5511@gmail.com>
2022-05-02 11:21:05 -0400

    Results now list of tuples

--- gatorgrade/output/output_percentage_testing.py
+++ gatorgrade/output/output_percentage_testing.py
@@ -1,9 +1,34 @@
 
 from turtle import right
+
+from sympy import total_degree
 from output_functions import receive_command
 
-global right
-results = [(2,True),(2,False),(4,True)] 
+
+results = [('Complete all TODOs', True, ''), ('Use an if statement', False, 'Found 0 match(es) of the regular expression in output or yayaya.py'), 
+    ('Complete all TODOs', True, ''), ('Use an if statement', False, 'Found 0 match(es) of the regular expression in output or module.py'), 
+    ('Have a total of 8 commits, 5 of which were created by you', True, '')]
+
+T_list = []
+Total = []
+for result in results:
+   checks = result[1]
+   for checks in checks:
+        T_list.append(checks)
+
+    
+    
+        if checks == True:
+            Total.append(checks)
+    
+
+
+
+
+print(len(T_list))
+print(len(Total))
+
+'''
 def find_total():
     total = 0
     for () in results:
@@ -17,8 +42,8 @@ def find_amount_right():
     for i in results(i[1]) == True:
         right = +1
     yield right    
-    
-
+'''
+'''
 print(f"{find_amount_right}{find_total}")
 
 
@@ -34,7 +59,7 @@ def print_all():
 
     
 
-    '''
+
 def percentage_from_check():
     for each command 
         look through the list of tuples 
@@ -45,41 +70,28 @@ def percentage_from_check():
         do math of number true vs total 
         bool / total len
         def math()
-'''
 
 
-'''
+
 total = 0
 for data, value in tuple1:
       total += value     #get the total
 for data,value in tuple1:
       print float(value) / total * 100
-'''
-'''
+
 tuple1 = [('Data1', 33), ('Data2', 52), ('Data3', 85)]
 total = (sum(t[1] for t in tuple1))
 for a, b in tuple1:
     y = b * 100 / total
     a_percent = a, y
     print(a_percent)
-'''
 
-'''
+
+
 list = []
 take the list  
 index list to take the str bool str 1,2,3 bc 0 is the file path/name
 list is made up of total command tuple with desc bool diagnostic if bool = false 
+
+
 '''
-
-
-def return_command_full_tuple():   
-    list = []
-    str1 = "no todos"
-    bool = False
-    str2 = "still have 3 todos"
-    tuple1 = (str1,bool,str2)
-    if bool == False: 
-        list.append(tuple1)
-        
-    print(list)
-return_command_full_tuple()
