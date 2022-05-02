9825fe4e05f515e0113246601b1c7f621d63cc88
Peyton Kelly <pkelly5511@gmail.com>
2022-05-02 16:59:25 -0400

    Deleting skeleton code

--- gatorgrade/output/output_percentage_testing.py
+++ gatorgrade/output/output_percentage_testing.py
@@ -18,83 +18,10 @@ def print_percentage():
     math = (len(True_list)/len(results))       
     Percent = math * 100
     if Percent == 100.0:
-        print(f"\n{Fore.GREEN}Passing all GatorGrader Checks {Percent}%\n")
+        print(f"{Fore.GREEN}|=====================================|\n|Passing all GatorGrader Checks {Percent}%|\n|=====================================|")
     else:
-        print(f"\n{Fore.RED}Passing {len(True_list)} of {len(results)}, Grade is {Percent}%\n")
+        print(f"\n{Fore.RED}Passing {len(True_list)} of {len(results)}, Grade is {Percent}%.\n")
 
 print_percentage()
 
 
-
-
-
-
-
-
-'''
-def find_total():
-    total = 0
-    for () in results:
-        total = +1
-    
-    yield total
-
-
-def find_amount_right():
-    right = 0
-    for i in results(i[1]) == True:
-        right = +1
-    yield right    
-'''
-'''
-print(f"{find_amount_right}{find_total}")
-
-
-def print_percentage():
-    find_total()
-    find_amount_right()
-
-
-def print_all():
-    print(f"\n\t\tPassing {find_amount_right(right)}/")
-
-            
-
-    
-
-
-def percentage_from_check():
-    for each command 
-        look through the list of tuples 
-        check bool 
-        take ones that are true 
-        append to list 
-        take ones false append to list 
-        do math of number true vs total 
-        bool / total len
-        def math()
-
-
-
-total = 0
-for data, value in tuple1:
-      total += value     #get the total
-for data,value in tuple1:
-      print float(value) / total * 100
-
-tuple1 = [('Data1', 33), ('Data2', 52), ('Data3', 85)]
-total = (sum(t[1] for t in tuple1))
-for a, b in tuple1:
-    y = b * 100 / total
-    a_percent = a, y
-    print(a_percent)
-
-
-
-list = []
-take the list  
-index list to take the str bool str 1,2,3 bc 0 is the file path/name
-list is made up of total command tuple with desc bool diagnostic if bool = false 
-
-
-'''
