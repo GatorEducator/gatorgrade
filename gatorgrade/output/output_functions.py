b391216e12169b3c5127a45be668d07bba28c9a8
Peyton Kelly <pkelly5511@gmail.com>
2022-05-02 14:58:23 -0400

    Added import to print percentage

--- gatorgrade/output/output_functions.py
+++ gatorgrade/output/output_functions.py
@@ -3,13 +3,17 @@
 
 import gator
 import os
+from gatorgrade.output.output_percentage_testing import print_percetage
 import output_tools
 
 # Commands are received as list of tuples.
 # Each tuple in the list is a string and list of strings <string, [strings]>
 # The string is the path to the file or filename and the list is the commands for the file.
 def receive_command(command_info):
-    results = []
+    results = [('Complete all TODOs', True, ''), ('Use an if statement', False, 'Found 0 match(es) of the regular expression in output or yayaya.py'), 
+    ('Complete all TODOs', True, ''), ('Use an if statement', False, 'Found 0 match(es) of the regular expression in output or module.py'), 
+    ('Have a total of 8 commits, 5 of which were created by you', True, '')]
+
 
     for file_name, commands in command_info:
         split_commands = []
@@ -29,5 +33,5 @@ def receive_command(command_info):
 
 
     # Here will be the code to send results to output functions ex:)
-    # print_percentage(results)
+    print_percetage(results)
     # print_description(results)
