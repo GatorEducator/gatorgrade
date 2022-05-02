074cbf4b9bcfd791f3d2379866abffe3b95506c1
gvanzin-allegheny <vanzing@allegheny.edu>
2022-05-02 00:13:59 -0400

    Run black on code

--- gatorgrade/output/test_output_functions.py
+++ gatorgrade/output/test_output_functions.py
@@ -28,8 +28,6 @@ def test_receive_command_function_returns_correct_results():
     }
 
     expected_result = [("Check TODOs", True, "")]
-    actual_result = output_functions.run_commands_and_return_results(
-        commands
-    )
+    actual_result = output_functions.run_commands_and_return_results(commands)
 
     assert expected_result == actual_result
