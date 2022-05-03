43d9c33ef4f5694de399de27340e47cee8e579da
Jacob-Allebach <allebach01@allegheny.edu>
2022-05-03 15:35:54 -0400

    Add some comments and check for setup commands

--- gatorgrade/input/in_file_path.py
+++ gatorgrade/input/in_file_path.py
@@ -12,23 +12,24 @@ def parse_yaml_file(file_path):
 def reformat_yaml_data(data):
     """Reformat the raw data from a YAML file into a list of tuples."""
     reformatted_data = []
-    data.pop(0)  # Removes the setup commands
+    if len(data) == 2:
+        data.pop(0)  # Removes the setup commands
     add_checks_to_list(None, data[0], reformatted_data)
     return reformatted_data
 
 
 def add_checks_to_list(path, data_list, reformatted_data):
     """Recursively loop through the data and add any checks that are found to the reformatted list."""
-    current_path = path
+    current_path = path #Saves the current path to keep track of the location
     for dict in data_list:
         for item in dict:
-            if isinstance(dict[item], list):
+            if isinstance(dict[item], list): #Checks if the current dictionary has another list as its value
                 if not path:
                     path = item
                 else:
                     path = f"{path}/{item}"
-                add_checks_to_list(path, dict[item], reformatted_data)
+                add_checks_to_list(path, dict[item], reformatted_data) #Runs this same function on the list inside of a dictionary
                 path = current_path
-            else:
+            else: #Adds the current check to the reformatted data list
                 reformatted_data.append({"file_context": path, "check": dict})
                 break
