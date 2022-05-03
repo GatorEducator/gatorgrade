9b7b4e0c5819578528cd102d400887a530108da5
Luke Lacaria <lacaria01@allegheny.edu>
2022-05-02 20:17:35 -0400

    Fix formatting for linting

--- gatorgrade/output/test_function.py
+++ gatorgrade/output/test_function.py
@@ -1,8 +1,11 @@
-from sys import stdout
-from output import output_functions
+""" Tests to ensure the output_functions.py functions work properly. """
+
+import output_functions
 import pytest
-from colorama import init, Fore, Style
+from colorama import init, Fore
+
 init()
+
 
 @pytest.fixture()
 def test_output_shows_green(capsys):
@@ -27,52 +30,49 @@ def test_output_shows_yellow(capsys):
     out, err = capsys.readouterr()
 
     assert f"{Fore.YELLOW}\u2192" in out
-    assert err ==  ""
+    assert err == ""
 
 
 def test_descrition_in_fail_message(capsys):
-    output_functions.output_check_result(file="txt",check=("todos",False,"no ifs "))
+    output_functions.output_check_result(file="txt", check=("todos", False, "no ifs "))
     out, err = capsys.readouterr()
     expected_output = "no ifs "
     actual_output = out
-    assert  expected_output in actual_output
-    assert "" in err 
+    assert expected_output in actual_output
+    assert "" in err
 
-def test_false_result_returns_X(capsys):
-    
-    output_functions.output_check_result(file="txt",check=("todos",False,"no ifs "))
-    
-    out,err = capsys.readouterr()
-    
-    assert '\u2718' in out
+
+def test_false_result_returns_x(capsys):
+
+    output_functions.output_check_result(file="txt", check=("todos", False, "no ifs "))
+
+    out, err = capsys.readouterr()
+
+    assert "\u2718" in out
     assert err == ""
-
 
 
 def test_style_reset():
 
-
-
-    '''
-def test_result():
-
+    """
+    def test_result():
 
 
 
 
 
 
-    
-
-Def test_passing_result_has_one_line 
-
-
-def test_output_will_show_diagnostic
-
-
-def test_Description_taken_as_argument
 
 
 
-def test style reset changes color
-'''
+    Def test_passing_result_has_one_line
+
+
+    def test_output_will_show_diagnostic
+
+
+    def test_Description_taken_as_argument
+
+
+
+    def test style reset changes color"""
