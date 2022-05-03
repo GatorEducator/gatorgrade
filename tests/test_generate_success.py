93dc6dd5abb9b277983c66a3c371ac17b04bf4c6
Anthony Grant-Cook <grantcooka@allegheny.edu>
2022-05-03 18:35:37 -0400

    Write tests for generate config

---
+++ tests/test_generate_success.py
@@ -0,0 +1,53 @@
+from gatorgrade.generate.generate import generate_config
+import pytest
+
+
+@pytest.fixture()
+def setup(tmp_path):
+    # Given this file structure
+    src_dir = tmp_path / "src"
+    src_dir.mkdir()
+    python_file = src_dir / "test.py"
+    python_file.write_text("import sys")
+    writing_dir = tmp_path / "writing"
+    writing_dir.mkdir()
+    reflection = writing_dir / "reflection.md"
+    reflection.write_text("# Reflection")
+    yield tmp_path
+
+
+def test_generate_config_creates_gatorgrade_yml(setup):
+    """Test to see that gatorgrade_yml file exist in file structure"""
+    # When generate_config is called
+    generate_config(["src"], setup)
+    # Then gatorgrade.yml is created
+    gatorgrade_yml = setup / "gatorgrade.yml"
+    assert gatorgrade_yml.is_file()
+
+
+def test_generate_config_creates_gatorgrade_yml_with_dir_in_user_input(setup):
+    """Test to see that gatorgrade_yml file exist in file structure"""
+    # When generate_config is called
+    generate_config(["src"], setup)
+    # Then gatorgrade.yml is created
+    gatorgrade_yml = setup / "gatorgrade.yml"
+    assert "src/test.py" in gatorgrade_yml.open().read()
+
+
+def test_generate_config_creates_gatorgrade_yml_without_dir_not_in_user_input(setup):
+    """Test to see that gatorgrade_yml file exist in file structure"""
+    # When generate_config is called
+    generate_config(["writing"], setup)
+    # Then gatorgrade.yml is created
+    gatorgrade_yml = setup / "gatorgrade.yml"
+    assert "src/test.py" not in gatorgrade_yml.open().read()
+    assert "writing/reflection.md" in gatorgrade_yml.open().read()
+
+
+def test_generate_success_message(capsys, setup):
+    """Test to see that there is a success message"""
+    # When generate_config is called
+    generate_config(["src"], setup)
+    out, err = capsys.readouterr()
+    # Then a success message is printed
+    assert "<Message>" in out
