cbc50a6c7c878baaa13eba5877376d076cafe628
MAbraham24 <70439403+MAbraham24@users.noreply.github.com>
2022-05-04 15:37:58 -0400

    Develop Parse Config Function

--- gatorgrade/input/parse_config_function.py
+++ gatorgrade/input/parse_config_function.py
@@ -1,6 +1,13 @@
-from command_line_generator import generate_checks
-from in_file_path import parse_yaml_file
+"""Returns the list of commands to be run through gatorgrader"""
+from command_line_generator import generate_checks # Import function to generate shell and gatorgrader checks
+from in_file_path import parse_yaml_file, reformat_yml_data, add_checks_to_list # Import functions to parse and set up yaml file 
 
-def parse_config():
-    """Parse yaml file and generate checks"""
-    return 
+def parse_config(file):
+    """Parse the input yaml file and generate specified checks
+    
+    Args:
+        file: Yaml file containing gatorgrade and shell command checks 
+    """
+    generate_checks(reformat_yaml_data(parse_yaml_file(file))) # Call previously generated function to modify file
+
+    # Create Args section in docstring
