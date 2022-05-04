11101241f21cc4070e793fcad9f01a61386f4a33
tuduun <gantulga01@allegheny.edu>
2022-05-04 13:44:51 -0400

    Remove testing

--- gatorgrade/input/test_in_file_path.py
+++ gatorgrade/input/test_in_file_path.py
@@ -1,17 +1,1 @@
-'''
-"""Test suite for command_line_generator.py"""
-
-import pytest
-from input import command_line_generator.py
-'''
-
-
-def test_generate_checks_with_file_paths():
-    """Test the checks."""
-
-    # Liam will write a test function here
-    # Given check commands are generated within a file path
-
-    # when generate_checks is run
-
-    # Then file path should be included in the list of strings
+"""Empty file."""
