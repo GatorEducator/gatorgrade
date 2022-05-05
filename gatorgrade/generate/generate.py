4cf45d1620962e84c987c559216e1c6c6b4d8512
Jack Turner <turner01@allegheny.edu>
2022-05-04 23:10:43 -0400

    Adding documentation

--- gatorgrade/generate/generate.py
+++ gatorgrade/generate/generate.py
@@ -16,33 +16,34 @@ def create_targeted_paths_list():
 
 def write_yaml_of_paths_list(path_names):  # expected input: A path list
     """Write YAML file to create gatorgrade file and set default messages."""
-    files_list = [
-        {
-            "path/to": [
-                {
-                    "file.py": [
-                        {
-                            "description": "Complete all TODOs",
-                            "check": "MatchFileFragment",
-                            "options": {"fragment": "TODO", "count": 0, "exact": True},
-                        }
-                    ]
-                },
-                {
-                    "file2.py": [
-                        {
-                            "description": "Complete all TODOs",
-                            "check": "MatchFileFragment",
-                            "options": {"fragment": "TODO", "count": 0, "exact": True},
-                        }
-                    ]
-                },
-            ]
-        }
-    ]
+
+    setup_dict = {
+        "setup": "# add setup commands here\n",
+    }
     # Write the default set up messages in YAML file.
 
-    with open('gatorgrade.yml', 'w') as file:
-        # Open the YAML file gatorgrade 
-        data = yaml.dump(files_list, file)
-        # Yaml dump accepts a python object and produces a YAML document.
+    path_names = create_targeted_paths_list(["test"])
+    # Set path_names 
+
+    files_list = []
+    # Create an empty list named files_list to store 
+    for file_path in path_names:
+        # Iterate through items in path_names
+        file_path_fixed = file_path.replace("./", "")
+        # Make file_path easier to rea by removing unnecessary characters
+        file_path_dict = {
+            file_path_fixed: [
+                {
+                    "description": "Complete all TODOs",
+                    "check": "MatchFileFragment",
+                    "options": {"fragment": "TODO", "count": 0, "exact": True},
+                }
+            ]
+        }
+        files_list.append(file_path_dict)
+        # Append files_list with the values stored inside file_path_dict
+
+    with open("gatorgrade.yml", "w") as file:
+        # Open a new YAML file named gatorgrade
+        data = yaml.dump(files_list, file, sort_keys=False)
+        # 
