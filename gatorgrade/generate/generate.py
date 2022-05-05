33467a41b40bff9a885739a95e517d6ce0da88b3
Yanqiao Chen <chen02@allegheny.edu>
2022-05-05 15:44:50 -0400

    Delete useless function
    The function shouldn't be in this barnch. Delete it.

--- gatorgrade/generate/generate.py
+++ gatorgrade/generate/generate.py
@@ -80,10 +80,3 @@ def create_targeted_paths_list(
         )
 
     return targeted_paths
-
-
-def write_yaml_of_paths_list():  # expected input: A path list
-    """Write YAML file to create gatorgrade file and set default messages."""
-    # Create a new YAML file with PyYaml in the specific path.
-    # Write the default set up messages in YAML file.
-    # List the file paths in specific format.
