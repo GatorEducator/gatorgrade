cabbe6ec6a0611cf3114fa4354e1edd909bc089c
Danny Ullrich <42869122+ullrichd21@users.noreply.github.com>
2022-05-06 13:34:26 -0400

    Fixed empty path bug.

--- docs/gen_pages.py
+++ docs/gen_pages.py
@@ -9,12 +9,12 @@ nav = mkdocs_gen_files.Nav()
 for path in sorted(Path("gatorgrade").rglob("*.py")):
     module_path = path.relative_to("gatorgrade").with_suffix("")
     doc_path = path.relative_to("gatorgrade").with_suffix(".md")
-    full_doc_path = Path("reference", doc_path)  #
+    full_doc_path = Path("reference", doc_path)
 
     parts = list(module_path.parts)
 
     if parts[-1] == "__init__":
-        parts = parts[:-1]
+        continue
     elif parts[-1] == "__main__":
         continue
 
