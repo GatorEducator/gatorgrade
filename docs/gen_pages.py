59ed384c6c414a4c866f7735a970c404172d61b9
Danny Ullrich <42869122+ullrichd21@users.noreply.github.com>
2022-05-06 12:06:56 -0400

    Remove unused code and left over comments.

--- docs/gen_pages.py
+++ docs/gen_pages.py
@@ -6,33 +6,28 @@ import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
 
-for path in sorted(Path("gatorgrade").rglob("*.py")):  #
-    module_path = path.relative_to("gatorgrade").with_suffix("")  #
-    doc_path = path.relative_to("gatorgrade").with_suffix(".md")  #
+for path in sorted(Path("gatorgrade").rglob("*.py")):
+    module_path = path.relative_to("gatorgrade").with_suffix("")
+    doc_path = path.relative_to("gatorgrade").with_suffix(".md")
     full_doc_path = Path("reference", doc_path)  #
 
     parts = list(module_path.parts)
 
-    if parts[-1] == "__init__":  #
+    if parts[-1] == "__init__":
         parts = parts[:-1]
     elif parts[-1] == "__main__":
         continue
 
     nav[parts] = doc_path.as_posix()
 
-    with mkdocs_gen_files.open(full_doc_path, "w") as fd:  #
-        identifier = "gatorgrade." + ".".join(parts)  #
-        print("::: " + identifier, file=fd)  #
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+        identifier = "gatorgrade." + ".".join(parts)
+        fd.write("::: " + identifier)
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, path)  #
+    mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
 #Generate navigation
 with mkdocs_gen_files.open("reference/Summary.md", "w") as nav_file:
-    # Generate source pages nav
-    # for file in sorted(Path("docs/source").rglob("*.md")):
-    #     title = file.relative_to("docs/source").with_suffix("")
-    #     path = file.relative_to("docs/")
-    #     print(f" * [{title}]({path})", file=nav_file)
 
     #Generate code reference navigation
     nav_file.writelines(nav.build_literate_nav())
