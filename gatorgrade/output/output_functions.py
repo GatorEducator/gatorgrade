433d7271457218cea920fd91f250cf129ad66584
Peyton Kelly <pkelly5511@gmail.com>
2022-05-06 09:40:20 -0400

    Returning bool to call

--- gatorgrade/output/output_functions.py
+++ gatorgrade/output/output_functions.py
@@ -43,11 +43,12 @@ def output_failed_checks(failed_checks):
     for i in failed_checks:
         # Extract the details of each check
         requirement = i[0]
+        bool = i[1]
         description = i[2]
         # Use colorama to print and style "X"
         print(f"{color.Fore.RED}\u2718  {color.Style.RESET_ALL}{requirement}")
         print(f"    {color.Fore.YELLOW}\u2192  {description}")
-
+        return bool
 
 # Display a sample output of how the function could display a result object from GatorGrader
 sample_result = [
