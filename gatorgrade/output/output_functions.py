345956ee319bdc80ec940c383204b232c45b7256
Luke Lacaria <lacaria01@allegheny.edu>
2022-05-07 11:12:20 -0400

    Fix docstring and extra line in main

--- gatorgrade/output/output_functions.py
+++ gatorgrade/output/output_functions.py
@@ -1,7 +1,8 @@
 """
-This class is used for storing the main functions requested from the
-Github Issue Tracker for the output team. For instance,
-functions dealing with percentage output, description output,
+This class is used for storing the main functions.
+
+The functions are requested from the Github Issue Tracker for the output team.
+For instance, functions dealing with percentage output, description output,
 and colorization of text.
 """
 import colorama as color
