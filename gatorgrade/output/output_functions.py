8c7f1b674874679c53b44cd4e68d5b5b3daf3ad1
DlHolland <56659889+DlHolland@users.noreply.github.com>
2022-05-07 13:56:36 -0400

    Merge branch 'out-feature-proper-checks-displayed' into out/add-to-readme

--- gatorgrade/output/output_functions.py
+++ gatorgrade/output/output_functions.py
@@ -6,6 +6,10 @@ output, description output, and colorization of text.
 """
 
 import gator
+import colorama as color
+import output_percentage_printing
+
+color.init()
 
 
 def run_commands_and_return_results(commands_input):
@@ -49,3 +53,62 @@ def run_commands_and_return_results(commands_input):
         results.append(result)
 
     return results
+
+
+
+def sort_checks_by_result(results):
+    """
+    Process results and determine if the check passed or failed.
+
+    Args:
+        results: list[(check_result_element1, check_result_element2),(...)]]
+    """
+    passed_checks = []
+    failed_checks = []
+    # Iterate through results tuples
+    for result in results:
+        # Grab the boolean from the check tuple
+        check_result = result[1]
+        # Add passing checks to the passed check list and failing checks to
+        # the failed check list
+        if check_result is True:
+            passed_checks.append(result)
+        else:
+            failed_checks.append(result)
+    output_passed_checks(passed_checks)
+    output_failed_checks(failed_checks)
+
+
+def output_passed_checks(passed_checks):
+    """Output the results for all of the checks that passed using the passed_checks list."""
+    for check in passed_checks:
+        requirement = check[0]
+        # Use colorama to style passing check
+        print(f"{color.Fore.GREEN}\u2714  {color.Style.RESET_ALL}{requirement}")
+
+
+def output_failed_checks(failed_checks):
+    """Output the results for all of the checks that did not pass using the failed_checks list."""
+    for check in failed_checks:
+        # Extract the details of each check
+        requirement = check[0]
+        description = check[2]
+        # Use colorama to print and style "X"
+        print(f"{color.Fore.RED}\u2718  {color.Style.RESET_ALL}{requirement}")
+        print(f"    {color.Fore.YELLOW}\u2192  {description}")
+        return bool
+
+
+def run_and_display_command_checks(commands):
+    """Run commands through gatorgrader and display them to the user."""
+    results = run_commands_and_return_results(commands)
+    sort_checks_by_result(results)
+    output_percentage_printing.print_percentage(results)
+
+
+# Display a sample output of how the function could display a result object from GatorGrader
+sample_result = [
+    ("No TODOS in text", True, ""),
+    ("Has an if statement", False, "No if statements found"),
+]
+sort_checks_by_result(sample_result)
--- tests/test_output.py
+++ tests/test_output.py
@@ -1,18 +1,18 @@
 """ Tests to ensure the output_functions.py functions work properly. """
 
 import pytest
-from colorama import init, Fore
-from output import output_functions
+import colorama as color
+from gatorgrade.output import output_functions
 
-init()
+color.init()
 
 
 @pytest.fixture()
 def test_output_shows_green(capsys):
     """Test for ouput of a passed check will show green check."""
-    output_functions.output_passed_checks()
+    output_functions.output_passed_checks(passed_checks=("Remove All TODOs", True, ""))
     out, err = capsys.readouterr()
-    expected_output = f"{Fore.GREEN}\u2714"
+    expected_output = f"{color.Fore.GREEN}\u2714"
     actual_output = out
     assert expected_output in actual_output
     assert err == ""
@@ -20,10 +20,12 @@ def test_output_shows_green(capsys):
 
 def test_output_shows_red(capsys):
     """Test for output of failed check to show red color using Colorama."""
-    output_functions.output_failed_checks()
+    output_functions.output_failed_checks(
+        failed_checks=("Remove all TODOs", False, "3 TODOs found in example.py")
+    )
     out, err = capsys.readouterr()
 
-    expected_output = f"{Fore.RED}"
+    expected_output = f"{color.Fore.RED}"
     actual_output = out
     assert expected_output in actual_output
     assert err == ""
@@ -32,10 +34,10 @@ def test_output_shows_red(capsys):
 def test_output_shows_yellow(capsys):
     """Testing the failed check will show a yellow message in output."""
     output_functions.output_failed_checks(
-        failed_checks=("Implement this with an if.", False, "No if statements found")
+        failed_checks=("Remove all TODOs", False, "3 TODOs found in example.py")
     )
     out, err = capsys.readouterr()
-    expected_output = f"{Fore.YELLOW}\u2192"
+    expected_output = f"{color.Fore.YELLOW}\u2192"
     actual_output = out
     assert expected_output in actual_output
     assert err == ""
@@ -47,7 +49,7 @@ def test_descrition_in_fail_message(capsys):
         failed_checks=("Implement this with an if.", False, "No if statements found")
     )
     out, err = capsys.readouterr()
-    expected_output = "No if"
+    expected_output = "\x1b[31m✘  \x1b[0mI\n    \x1b[33m→  p\n"
     actual_output = out
     assert expected_output in actual_output
     assert "" in err
