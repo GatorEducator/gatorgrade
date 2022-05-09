a28d68869bf70f610d220a98c5fead8070210444
MAbraham24 <70439403+MAbraham24@users.noreply.github.com>
2022-05-09 09:31:33 -0400

    Run Black and Lint

--- gatorgrade/input/parse_config.py
+++ gatorgrade/input/parse_config.py
@@ -1,12 +1,21 @@
 """Returns the list of commands to be run through gatorgrader"""
-from command_line_generator import generate_checks # Import function to generate shell and gatorgrader checks
-from in_file_path import parse_yaml_file, reformat_yml_data, add_checks_to_list # Import functions to parse and set up yaml file 
+from command_line_generator import (
+    generate_checks,
+)  # Import function to generate shell and gatorgrader checks
+from in_file_path import (
+    parse_yaml_file,
+    reformat_yml_data,
+    add_checks_to_list,
+)  # Import functions to parse and set up yaml file
+
 
 def parse_config(file):
     """Parse the input yaml file and generate specified checks
-    
+
     Args:
-        file: Yaml file containing gatorgrade and shell command checks 
+        file: Yaml file containing gatorgrade and shell command checks
     """
-    parse_config = generate_checks(reformat_yaml_data(parse_yaml_file(file))) # Call previously generated function to modify file
+    parse_config = generate_checks(
+        reformat_yaml_data(parse_yaml_file(file))
+    )  # Call previously generated function to modify file
     return parse_config
