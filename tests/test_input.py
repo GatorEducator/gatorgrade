52d41e604855e867ff3a5f01e68a89a00ceef692
MAbraham24 <70439403+MAbraham24@users.noreply.github.com>
2022-05-09 09:51:50 -0400

    Fix Test Input Errors

--- tests/test_input.py
+++ tests/test_input.py
@@ -1,7 +1,9 @@
-from gatorgrade.input.in_file_path import parse_config
+"""Tests the parse_config functions """
+from gatorgrade.input.parse_config import parse_config
 
 
 def test_parse_config_gg_check_in_file_context_contains_file():
+    """Test whether the input file can run through parse config"""
     config = "tests/input/gatorgrade_one_gg_check_in_file_context.yml"
     output = parse_config(config)
-    assert "file.py" in output[0]
+    assert "file.py" in output[0]   
