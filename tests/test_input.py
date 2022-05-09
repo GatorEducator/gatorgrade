8ad4a75740ddb7102108a391ceea64979b1247fb
MAbraham24 <70439403+MAbraham24@users.noreply.github.com>
2022-05-09 09:55:59 -0400

    Black and Lint Test Input

--- tests/test_input.py
+++ tests/test_input.py
@@ -6,4 +6,4 @@ def test_parse_config_gg_check_in_file_context_contains_file():
     """Test whether the input file can run through parse config"""
     config = "tests/input/gatorgrade_one_gg_check_in_file_context.yml"
     output = parse_config(config)
-    assert "file.py" in output[0]   
+    assert "file.py" in output[0]
