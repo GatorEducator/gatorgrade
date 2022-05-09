ae4377636cd50790a8eae7ed5f1b2009ac997393
Jack Turner <turner01@allegheny.edu>
2022-05-09 11:38:05 -0400

    Added generate config function

--- gatorgrade/generate/generate.py
+++ gatorgrade/generate/generate.py
@@ -3,7 +3,7 @@
 Gatorgrade file will include paths to whitelisted files
 and default GatorGrader checks.
 """
-
+from typing import List
 # Import the necessary libraries
 
 
@@ -19,3 +19,10 @@ def write_yaml_of_paths_list():  # expected input: A path list
     # Create a new YAML file with PyYaml in the specific path.
     # Write the default set up messages in YAML file.
     # List the file paths in specific format.
+
+
+def generate_config(target_path_list: List[str], relative_run_path: str = "."):
+    """Generate config by creating targeted paths in a list of strings, then writing the list of strings to a YAML file"""
+    targeted_paths = create_targeted_paths_list(target_path_list, relative_run_path)
+    write_yaml_of_paths_list(targeted_paths)
+    
