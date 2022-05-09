d01ef0aad9722b82f5ce5b5973118d71395a5ef5
Danny Ullrich <42869122+ullrichd21@users.noreply.github.com>
2022-05-09 11:05:09 -0400

    Make nav file generation more clear/simpler.

--- docs/gen_pages.py
+++ docs/gen_pages.py
@@ -21,8 +21,7 @@ for path in sorted(Path("gatorgrade").rglob("*.py")):
     nav[parts] = doc_path.as_posix()
 
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-        identifier = "gatorgrade."+".".join(parts)
-        print("::: " + identifier, file=fd)
+        fd.write(f"::: {'gatorgrade.' +'.'.join(parts)}")
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
