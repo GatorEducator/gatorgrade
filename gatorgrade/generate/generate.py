52a3f1857efc8ed491535b80b7e816b03a7f61cc
Ochirsaikhan <davaajambal01@allegheny.edu>
2022-05-11 16:37:48 -0400

    Fix linting issue

--- gatorgrade/generate/generate.py
+++ gatorgrade/generate/generate.py
@@ -52,7 +52,9 @@ def create_targeted_paths_list(
             complete_actual_path = os.path.join(dirpath, filename) + os.path.sep
             for target in corrected_paths:
                 if target in complete_actual_path:
-                    polished_paths = complete_actual_path.replace(f"..{os.path.sep}", "")
+                    polished_paths = complete_actual_path.replace(
+                        f"..{os.path.sep}", ""
+                    )
                     targeted_paths.append(polished_paths)
 
     # If any of the user inputted file does not exist in any directory,
