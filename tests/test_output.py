c3c19b87591cce5f1da19fd48a4de0b005495adb
Saejin Heinert <saejinmh@gmail.com>
2022-05-19 15:22:22 -0400

    Remove color tests in `test_output.py`

--- tests/test_output.py
+++ tests/test_output.py
@@ -1,46 +1,9 @@
 """ Tests to ensure the output_functions.py functions work properly. """
 
-import pytest
 from gatorgrade.output import output_functions
 
 
-def test_output_shows_green(capsys):
-    """Test for ouput of a passed check will show green check."""
-    output_functions.output_passed_checks([("Remove All TODOs", True, "")])
-    out, err = capsys.readouterr()
-    expected_output = f"\u2714"
-
-    actual_output = out
-    assert expected_output in actual_output
-    assert err == ""
-
-
-def test_output_shows_red(capsys):
-    """Test for output of failed check to show red color using Colorama."""
-    output_functions.output_failed_checks(
-        [("Remove all TODOs", False, "3 TODOs found in example.py")]
-    )
-    out, err = capsys.readouterr()
-
-    expected_output = f""
-    actual_output = out
-    assert expected_output in actual_output
-    assert err == ""
-
-
-def test_output_shows_yellow(capsys):
-    """Testing the failed check will show a yellow message in output."""
-    output_functions.output_failed_checks(
-        [("Remove all TODOs", False, "3 TODOs found in example.py")]
-    )
-    out, err = capsys.readouterr()
-    expected_output = f"\u2192"
-    actual_output = out
-    assert expected_output in actual_output
-    assert err == ""
-
-
-def test_descrition_in_fail_message(capsys):
+def test_description_in_fail_message(capsys):
     """Testing the failed check will show diagnostic yellow message in output."""
     output_functions.output_failed_checks(
         [("Implement this with an if.", False, "No if statements found")]
