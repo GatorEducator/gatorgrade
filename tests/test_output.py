8e35f2437b2f53fe0c9d606be97de880881fe7b4
Saejin Heinert <saejinmh@gmail.com>
2022-05-19 13:49:39 -0400

    Fix `test_output.py` inputs to use list of tuples instead of single tuple
    Also fix expected output in `test_descrition_in_fail_message`

--- tests/test_output.py
+++ tests/test_output.py
@@ -10,7 +10,7 @@ color.init()
 @pytest.fixture()
 def test_output_shows_green(capsys):
     """Test for ouput of a passed check will show green check."""
-    output_functions.output_passed_checks(passed_checks=("Remove All TODOs", True, ""))
+    output_functions.output_passed_checks([("Remove All TODOs", True, "")])
     out, err = capsys.readouterr()
     expected_output = f"{color.Fore.GREEN}\u2714"
 
@@ -22,7 +22,7 @@ def test_output_shows_green(capsys):
 def test_output_shows_red(capsys):
     """Test for output of failed check to show red color using Colorama."""
     output_functions.output_failed_checks(
-        failed_checks=("Remove all TODOs", False, "3 TODOs found in example.py")
+        [("Remove all TODOs", False, "3 TODOs found in example.py")]
     )
     out, err = capsys.readouterr()
 
@@ -35,7 +35,7 @@ def test_output_shows_red(capsys):
 def test_output_shows_yellow(capsys):
     """Testing the failed check will show a yellow message in output."""
     output_functions.output_failed_checks(
-        failed_checks=("Remove all TODOs", False, "3 TODOs found in example.py")
+        [("Remove all TODOs", False, "3 TODOs found in example.py")]
     )
     out, err = capsys.readouterr()
     expected_output = f"{color.Fore.YELLOW}\u2192"
@@ -47,10 +47,10 @@ def test_output_shows_yellow(capsys):
 def test_descrition_in_fail_message(capsys):
     """Testing the failed check will show diagnostic yellow message in output."""
     output_functions.output_failed_checks(
-        failed_checks=("Implement this with an if.", False, "No if statements found")
+        [("Implement this with an if.", False, "No if statements found")]
     )
     out, err = capsys.readouterr()
-    expected_output = "\x1b[31m✘  \x1b[0mI\n    \x1b[33m→  p\n"
+    expected_output = f"{color.Fore.YELLOW}→  No if statements found"
     actual_output = out
     assert expected_output in actual_output
     assert "" in err
@@ -59,7 +59,7 @@ def test_descrition_in_fail_message(capsys):
 def test_false_result_returns_x(capsys):
     """Test will result a X mark in the output for the failed check."""
     output_functions.output_failed_checks(
-        failed_checks=("Implement this with an if.", False, "No if statements found")
+        [("Implement this with an if.", False, "No if statements found")]
     )
     out, err = capsys.readouterr()
     unicode_x = "\u2718"
