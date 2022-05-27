914d27e92808e77bf67857c33f1222a3c59eb0fa
Yanqiao Chen <chen02@allegheny.edu>
2022-05-27 13:07:36 -0400

    Fix generate test bug
    The test mistakenly called the source
    codes with wrong types of inputs. Fix it

--- tests/test_generate_success.py
+++ tests/test_generate_success.py
@@ -2,6 +2,7 @@
 """Test the generate_config function in a perfect situation."""
 import os
 import pytest
+from pathlib import Path
 from gatorgrade.generate.generate import generate_config
 
 
@@ -26,7 +27,7 @@ def setup_files(tmp_path):
 def test_generate_config_create_gatorgrade_yml(testing_dir):
     """Test to see that gatorgrade_yml file exist in file structure"""
     # When generate_config is called
-    generate_config(["src"], testing_dir)
+    generate_config(["src"], str(testing_dir))
     # Then gatorgrade.yml is created
     gatorgrade_yml = testing_dir / "gatorgrade.yml"
     assert gatorgrade_yml.is_file()
@@ -35,7 +36,7 @@ def test_generate_config_create_gatorgrade_yml(testing_dir):
 def test_generate_config_creates_gatorgrade_yml_with_dir_in_user_input(testing_dir):
     """Test to see if input matches directory"""
     # When generate_config is called
-    generate_config(["src"], testing_dir)
+    generate_config(["src"], str(testing_dir))
     # Then gatorgrade.yml is created
     gatorgrade_yml = testing_dir / "gatorgrade.yml"
     assert os.path.normpath("src/test.py") in gatorgrade_yml.open().read()
@@ -46,7 +47,7 @@ def test_generate_config_creates_gatorgrade_yml_without_dir_not_in_user_input(
 ):
     """Test to see if input does not match directory"""
     # When generate_config is called
-    generate_config(["writing"], testing_dir)
+    generate_config(["writing"], str(testing_dir))
     # Then gatorgrade.yml is created
     gatorgrade_yml = testing_dir / "gatorgrade.yml"
     assert os.path.normpath("src/test.py") not in gatorgrade_yml.open().read()
@@ -56,7 +57,7 @@ def test_generate_config_creates_gatorgrade_yml_without_dir_not_in_user_input(
 def test_generate_success_message(capsys, testing_dir):
     """Test to see that there is a success message"""
     # When generate_config is called
-    generate_config(["src"], testing_dir)
+    generate_config(["src"], str(testing_dir))
     out, _ = capsys.readouterr()
     # Then a success message is printed
     assert "SUCCESS" in out
