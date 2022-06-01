951438867eccd274778ecd20dbf571ec137100cd
Saejin Heinert <saejinmh@gmail.com>
2022-06-01 13:05:03 -0400

    Add basic generate command call

--- gatorgrade/main.py
+++ gatorgrade/main.py
@@ -1,8 +1,13 @@
 """Use Typer to run gatorgrade to run the checks and generate the yml file."""
+
+from typing import List
 from pathlib import Path
+
 import typer
+
 from gatorgrade.input.parse_config import parse_config
 from gatorgrade.output.output_functions import run_and_display_command_checks
+from gatorgrade.generate.generate import generate_config
 
 app = typer.Typer(add_completion=False)
 FILE = "gatorgrade.yml"
@@ -21,9 +26,23 @@ def gatorgrade(
 
 
 @app.command()
-def generate():
+def generate(
+    root: Path = typer.Argument(
+        Path("."),
+        help="Root directory of the assignment",
+        exists=True,
+        dir_okay=True,
+        writable=True,
+    ),
+    paths: List[str] = typer.Option(
+        ["."],
+        help="Paths to recurse through and generate checks for",
+    ),
+):
     """Generate a gatorgrade.yml file."""
-    pass  # pylint: disable=unnecessary-pass
+    targets = [Path(p).as_posix() for p in paths]
+    typer.echo(f"Root: {root}; Targets: {targets}")
+    generate_config(targets, root.as_posix())
 
 
 if __name__ == "__main__":
