7cfdc092fde80b38919209b70cf96fcaf4e718de
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-08-27 12:04:43 -0400

    fix: Use a different checkmark in the test_output test suite.

--- tests/output/test_output.py
+++ tests/output/test_output.py
@@ -30,7 +30,7 @@ def test_run_checks_gg_check_should_show_passed(capsys):
 
     # Then the output shows that the check has passed
     out, _ = capsys.readouterr()
-    assert "✔  Check TODOs" in out
+    assert "  Check TODOs" in out
 
 
 def test_run_checks_invalid_gg_args_prints_exception(capsys):
