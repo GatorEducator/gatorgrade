1dbf84e4b86af764ad928bf2b1023a055bfee372
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-08-27 12:59:39 -0400

    style: Reformat the from statements in the parse_config module.

--- gatorgrade/input/parse_config.py
+++ gatorgrade/input/parse_config.py
@@ -1,9 +1,11 @@
 """Returns the list of commands to be run through gatorgrader."""
 
 from gatorgrade.input.command_line_generator import (
-    generate_checks,)  # Import function to generate shell and gatorgrader checks
+    generate_checks,
+)
 from gatorgrade.input.in_file_path import (
-    parse_yaml_file,)  # Import functions to parse and set up yaml file
+    parse_yaml_file,
+)
 from gatorgrade.input.in_file_path import reformat_yaml_data
 
 
