c2bff3c0da11798878a4c2c54adf0cd6ab1ed8f7
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-08-27 14:59:23 -0400

    fix: Correct and add comments to parse_config function in parse_config module.

--- gatorgrade/input/parse_config.py
+++ gatorgrade/input/parse_config.py
@@ -1,11 +1,13 @@
 """Returns the list of commands to be run through gatorgrader."""
 
+from pathlib import Path
+
 from gatorgrade.input.command_line_generator import generate_checks
 from gatorgrade.input.in_file_path import parse_yaml_file
 from gatorgrade.input.in_file_path import reformat_yaml_data
 
 
-def parse_config(file):
+def parse_config(file: Path):
     """Parse the input yaml file and generate specified checks.
 
     Args:
@@ -13,7 +15,17 @@ def parse_config(file):
     Returns:
         Returns a dictionary that specifies shell commands and gatorgrade commands
     """
-    parse_con = generate_checks(
-        reformat_yaml_data(parse_yaml_file(file))
-    )  # Call previously generated function to modify file
-    return parse_con
+    parsed_yaml_file = parse_yaml_file(file)
+    if len(parsed_yaml_file) > 0:
+        # after reformatting the parse YAML file,
+        # use it to generate all of the checks;
+        # these will be valid checks that are now
+        # ready for execution with this tool
+        parse_con = generate_checks(
+            reformat_yaml_data(parsed_yaml_file)
+        )
+        return parse_con
+    # return an empty list because of the fact that the
+    # parsing process did not return a list with content;
+    # allow the calling function to handle the empty list
+    return []
