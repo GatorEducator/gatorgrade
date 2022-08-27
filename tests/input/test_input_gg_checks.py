51a28df255f1b0a5411226ad5a8e3139b346e9f9
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-08-27 15:04:41 -0400

    fix: Correct all tests in test_input_gg_checks suite that passed an str instead of a pathlib Path to the parse_config function.

--- tests/input/test_input_gg_checks.py
+++ tests/input/test_input_gg_checks.py
@@ -1,5 +1,7 @@
 """Test suite for parse_config function."""
 
+from pathlib import Path
+
 from gatorgrade.input.checks import GatorGraderCheck
 from gatorgrade.input.checks import ShellCheck
 from gatorgrade.input.parse_config import parse_config
@@ -8,7 +10,7 @@ from gatorgrade.input.parse_config import parse_config
 def test_parse_config_gg_check_in_file_context_contains_file():
     """Test to make sure that the file context is included in the GatorGrader arguments."""
     # Given a configuration file with a GatorGrader check within a file context
-    config = "tests/input/yml_test_files/gatorgrade_one_gg_check_in_file.yml"
+    config = Path("tests/input/yml_test_files/gatorgrade_one_gg_check_in_file.yml")
     # When parse_config is run
     output = parse_config(config)
     # Then the file path should be in the GatorGrader arguments
@@ -18,7 +20,7 @@ def test_parse_config_gg_check_in_file_context_contains_file():
 def test_parse_config_check_gg_matchfilefragment():
     """Test to make sure the description, check name, and options appear in the GatorGrader arguments."""
     # Given a configuration file with a GatorGrader check
-    config = "tests/input/yml_test_files/gatorgrade_matchfilefragment.yml"
+    config = Path("tests/input/yml_test_files/gatorgrade_matchfilefragment.yml")
     # When parse_config is run
     output = parse_config(config)
     # Then the description, check name, and options appear in the GatorGrader arguments
@@ -41,7 +43,9 @@ def test_parse_config_check_gg_matchfilefragment():
 def test_parse_config_gg_check_no_file_context_contains_no_file():
     """Test to make sure checks without a file context do not have a file path in GatorGrader arguments."""
     # Given a configuration file with a GatorGrader check without a file context
-    config = "tests/input/yml_test_files/gatorgrade_one_gg_check_no_file_context.yml"
+    config = Path(
+        "tests/input/yml_test_files/gatorgrade_one_gg_check_no_file_context.yml"
+    )
     # When parse_config is run
     output = parse_config(config)
     # Then the GatorGrader arguments do not contain a file path
@@ -57,7 +61,7 @@ def test_parse_config_gg_check_no_file_context_contains_no_file():
 def test_parse_config_parses_both_shell_and_gg_checks():
     """Test to make sure that both shell and GatorGrader checks are parsed."""
     # Given a configuration file that contains a shell check and GatorGrader check
-    config = "tests/input/yml_test_files/gatorgrader_both_checks.yml"
+    config = Path("tests/input/yml_test_files/gatorgrader_both_checks.yml")
     # When parse_config is run
     output = parse_config(config)
     # Then the output should contain a shell check and GatorGrader check
@@ -68,7 +72,7 @@ def test_parse_config_parses_both_shell_and_gg_checks():
 def test_parse_config_yml_file_runs_setup_shell_checks():
     """Test to make sure that a configuration file without setup commands can be parsed."""
     # Given a configuration file without setup commands
-    config = "tests/input/yml_test_files/gatorgrade_no_shell_setup_check.yml"
+    config = Path("tests/input/yml_test_files/gatorgrade_no_shell_setup_check.yml")
     # When parse_config run
     output = parse_config(config)
     # Then the output should contain the GatorGrader check
@@ -84,7 +88,7 @@ def test_parse_config_yml_file_runs_setup_shell_checks():
 def test_parse_config_shell_check_contains_command():
     """Test to make sure that the command for a shell check is stored."""
     # Given a configuration file with a shell check
-    config = "tests/input/yml_test_files/gatorgrade_one_shell_command_check.yml"
+    config = Path("tests/input/yml_test_files/gatorgrade_one_shell_command_check.yml")
     # When the parse_config is run
     output = parse_config(config)
     # Then the command should be stored in the shell check
