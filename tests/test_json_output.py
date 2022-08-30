bc800ed04b56e26526403638125e5fa1c9558e30
Danny Ullrich <42869122+ullrichd21@users.noreply.github.com>
2022-08-30 10:25:21 -0400

    Fix linting.

--- tests/test_json_output.py
+++ tests/test_json_output.py
@@ -8,11 +8,10 @@ from gatorgrade import main
 
 runner = CliRunner()
 
+
 @pytest.mark.parametrize(
     "assignment_path",
-    [
-        ("tests/test_assignment")
-    ],
+    [("tests/test_assignment")],
 )
 def test_report_flag_creates_file_output(assignment_path, tmp_path, chdir):
     """Check if json output creates an output .json file after it's run"""
@@ -22,15 +21,15 @@ def test_report_flag_creates_file_output(assignment_path, tmp_path, chdir):
     file_path = tmp_path / "output.json"
     assert file_path.is_file()
 
+
 @pytest.mark.parametrize(
     "assignment_path",
-    [
-        ("tests/test_assignment")
-    ],
+    [("tests/test_assignment")],
 )
 def test_report_flag_creates_valid_json_file_output(assignment_path, tmp_path, chdir):
     """Check if json output creates an output .json file after it's run"""
     import json
+
     chdir(assignment_path)
     runner.invoke(main.app, ["--report", f"{tmp_path}/output.json"])
     # "output.json" is generated in the root directory
@@ -38,4 +37,4 @@ def test_report_flag_creates_valid_json_file_output(assignment_path, tmp_path, c
     try:
         json.loads(open(file_path).read())
     except ValueError:
-        pytest.fail(f"\"{file_path}\" is not valid JSON.")
+        pytest.fail(f'"{file_path}" is not valid JSON.')
