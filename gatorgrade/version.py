e95731aa91f72ba34a458fda81225030207f454f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-09-09 12:22:10 -0400

    feat: Add get_project_version and refactor version functions.

--- gatorgrade/version.py
+++ gatorgrade/version.py
@@ -2,14 +2,23 @@
 
 from importlib.metadata import version
 
+COLON = ":"
+SPACE = " "
+
+
+def get_project_version(project: str) -> str:
+    """Extract the version information for a project and return formatted version string."""
+    project_version_str = version(project)
+    return project + COLON + SPACE + project_version_str
+
 
 def get_gatorgrade_version() -> str:
     """Determine and return the information about GatorGrade's version."""
-    gatorgrade_version_str = version("gatorgrade")
+    gatorgrade_version_str = get_project_version("gatorgrade")
     return gatorgrade_version_str
 
 
 def get_gatorgrader_version() -> str:
     """Determine and return the information about GatorGrade's version."""
-    gatorgrader_version_str = version("gatorgrader")
+    gatorgrader_version_str = get_project_version("gatorgrader")
     return gatorgrader_version_str
