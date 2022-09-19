87d45878284221288ae0241fbba29230fcc4fdf2
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-09-18 21:57:21 -0400

    coms: Add comments to explain steps in get_github_projects in github.

--- gatorgrade/util/github.py
+++ gatorgrade/util/github.py
@@ -16,7 +16,12 @@ PROJECTS = {
 
 def get_github_projects(projects: Dict[str, str] = PROJECTS) -> str:
     """Create a GitHub repository string for all specified projects."""
+    # create an empty project_version_str that will be appended
+    # to with information about each project and its GitHub URL
     project_version_str = ""
+    # iterate through the provided projects and display:
+    # 1) the name of the project
+    # 2) the GitHub URL for the project
     for project_name in projects.keys():
         current_project_version_str = (
             DASH
