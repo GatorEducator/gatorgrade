bf3b31f811a59c25c818faa02ada3046e1e6562e
burgess01 <burgess01@allegheny.edu>
2023-02-01 16:45:11 -0500

    feature: change any func call to all

--- gatorgrade/output/output.py
+++ gatorgrade/output/output.py
@@ -263,7 +263,7 @@ def run_checks(
         percent = round(passed_count / len(results) * 100)
 
     # if the report is wanted, create output in line with their specifications
-    if any(report):
+    if all(report):
         report_output_data = create_report_json(passed_count, checks, results, percent)
         configure_report(report, report_output_data)
 
