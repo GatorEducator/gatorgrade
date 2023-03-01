43fd60e008065061d3357ac2edc534b9282ce20c
burgess01 <burgess01@allegheny.edu>
2023-02-28 20:51:57 -0500

    feature: incorporate deadlines in main

--- gatorgrade/main.py
+++ gatorgrade/main.py
@@ -40,11 +40,11 @@ def gatorgrade(
     # that, by default, gatorgrade should run in checking mode
     if ctx.invoked_subcommand is None:
         # parse the provided configuration file
-        checks = parse_config(filename)
+        checks, deadline = parse_config(filename)
         # there are valid checks and thus the
         # tool should run them with run_checks
         if len(checks) > 0:
-            checks_status = run_checks(checks)
+            checks_status = run_checks(checks, deadline)
         # no checks were created and this means
         # that, most likely, the file was not
         # valid and thus the tool cannot run checks
