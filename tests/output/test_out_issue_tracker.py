1a834735e6afe35635de32e43f9550f7e94dbe2d
Yanqiao4396 <chen02@allegheny.edu>
2023-05-22 13:29:16 -0400

    #Feat: Add test for issue tracker

--- tests/output/test_out_issue_tracker.py
+++ tests/output/test_out_issue_tracker.py
@@ -1,5 +1,5 @@
 from tests.output.mock_api import github_mock_api
-from gatorgrade.output.out_issue_tracker import issueReport, issueExecute
+from gatorgrade.output.out_issue_tracker import issueReport, issueExecute,issueMode
 import pytest
 from pathlib import Path
 import yaml
@@ -13,7 +13,7 @@ from gatorgrade.input.in_file_path import parse_yaml_file
 @pytest.fixture
 def mocked_issueReport(monkeypatch):
     def mock_authenticate(*args, **kwargs):
-        return github_mock_api.MockGH, "my_repo"
+        return github_mock_api.MockGH(), "test_repo"
 
     monkeypatch.setattr(issueReport, "_authenticate", mock_authenticate)
     # Return a lambda function that instantiates MyClass with the patched attribute
@@ -106,7 +106,96 @@ def test_wrong_user_mode_should_fail(mocked_issueReport,tmp_path):
     assert e.type == SystemExit
     assert e.value.code == 1
 
+def test_issueReport_reports_successfully_without_option(mocked_issueReport,tmp_path):
+    """Test report method generates an issue successfully without any option being specified in config file."""
+    tmp_config = Path(tmp_path/"config_valid.yml")
 
+    # Given there is issue_report key in the configuration file
+    config_content = {
+        "setup": None,
+        "issue_report": {
+            "mode": "comment",
+        }
+    }
+
+    # Create a temporary file for test purpose
+    with open(tmp_config, "w") as file:
+        yaml.dump(config_content,file)
+
+    mocked_instance = mocked_issueReport(tmp_config,"Hello World")
+    assert mocked_instance.repo_name == "test_repo"
+    # report method should return True
+    assert mocked_instance.report()
+
+def test_issueReports_fails_without_user_data(mocked_issueReport,tmp_path):
+    """Test the report method should fail without any issue_report inf in configuration file."""
+    tmp_config = Path(tmp_path/"config_invalid.yml")
+
+    # Given there is no issue report inf specified in configuration file
+    config_content = {
+        "setup": None,
+    }
+
+    # Create a temporary file for test purpose
+    with open(tmp_config, "w") as file:
+        yaml.dump(config_content,file)
+
+    mocked_instance = mocked_issueReport(tmp_config,"Hello World")
+    assert mocked_instance.repo_name == "test_repo"
+    assert mocked_instance.report() == False
+
+def test_issueReport_reports_successfully_with_option(mocked_issueReport,tmp_path):
+    """Test report method generates an issue successfully with options being specified in config file."""
+    tmp_config = Path(tmp_path/"config_valid.yml")
+
+    # Given specific options in config file
+    config_content = {
+        "setup": None,
+        "issue_report": {
+            "mode": "multi",
+            "options": {
+                "issue_name": "Gatorgrade: Insight Report from YAML",
+                "labels": ["Hello-World", "YAMLtmd"],
+            },
+        }
+    }
+
+    # Create a temporary file for test purpose
+    with open(tmp_config, "w") as file:
+        yaml.dump(config_content,file)
+
+    mocked_instance = mocked_issueReport(tmp_config,"Hello World")
+    assert mocked_instance.repo_name == "test_repo"
+
+    # report method should return True
+    assert mocked_instance.report()
+
+def test_issueReport_report_fails_with_wrong_mode(mocked_issueReport,tmp_path):
+    """Test the issue reports falsely with a wrong mode other than the three supported modes."""
+    tmp_config = Path(tmp_path/"config_valid.yml")
+
+    # Given a wrong mode in issue report
+    config_content = {
+        "setup": None,
+        "issue_report": {
+            "mode": "wrong",
+            "options": {
+                "issue_name": "Gatorgrade: Insight Report from YAML",
+                "labels": ["Hello-World", "YAMLtmd"],
+            },
+        }
+    }
+
+    # Create a temporary file for test purpose
+    with open(tmp_config, "w") as file:
+        yaml.dump(config_content,file)
+
+    # When trying to call report method
+    with pytest.raises(SystemExit) as exit_inf:
+        mocked_instance = mocked_issueReport(tmp_config,"Hello World")
+        mocked_instance.report()
+    # A exit code 1 will be raised
+    assert exit_inf.value.code == 1
 ###########################
 #### issueExecute test ####
 ###########################
@@ -175,3 +264,57 @@ def test_update_issue_pass():
     assert found_issue and found_comment
     # Then the return value of rewrite_issue should be True
     assert status
+
+
+###########################
+##### issueMode test ######
+###########################
+
+def test_multi_issue_mode_pass():
+    """Check the multi_issue_mode function in class issueMode works well."""
+    github_mock_object = github_mock_api.MockGH()
+    issue_mode_instance = issueMode(api_object = github_mock_object,repo_name="test_repo")
+    issue_mode_instance.multi_issue_mode(issue_name="Insight",issue_body="hello world")
+    assert github_mock_object.repos["test_repo"]
+
+def test_rewrite_issue_mode_creates_new_issue():
+    """Check the rewrite_issue_mode function in class issueMode creates a new issue."""
+    github_mock_object = github_mock_api.MockGH()
+    issue_mode_instance = issueMode(api_object = github_mock_object,repo_name="test_repo")
+    create_issue_flag = issue_mode_instance.rewrite_issue_mode(issue_name="Insight",issue_body="hello world")
+    assert create_issue_flag == True
+
+def test_rewrite_issue_mode_rewrites_old_issue():
+    """Check the rewrite_issue_mode function in class issueMode rewrites a old issue instead of creating a new issue."""
+    github_mock_object = github_mock_api.MockGH()
+    issue_mode_instance = issueMode(api_object = github_mock_object,repo_name="test_repo")
+    # First time call rewrite_issue_mode
+    create_issue_flag = issue_mode_instance.rewrite_issue_mode(issue_name="Insight",issue_body="hello world")
+    # a new issue should be generated
+    assert create_issue_flag
+    # Second time call rewrite_issue_mode
+    create_issue_flag = issue_mode_instance.rewrite_issue_mode(issue_name="Insight",issue_body="hello world")
+
+    # An old issue should be rewritten and the method should return false
+    assert create_issue_flag == False
+
+def test_comment_issue_mode_creates_new_issue():
+    """Check the rewrite_issue_mode function in class issueMode creates a new issue."""
+    github_mock_object = github_mock_api.MockGH()
+    issue_mode_instance = issueMode(api_object = github_mock_object,repo_name="test_repo")
+    create_issue_flag = issue_mode_instance.comment_issue_mode(issue_name="Insight",issue_body="hello world")
+    assert create_issue_flag == True
+
+def test_comment_issue_mode_creates_comments_old_issue():
+    """Check the rewrite_issue_mode function in class issueMode comments a old issue instead of creating a new issue."""
+    github_mock_object = github_mock_api.MockGH()
+    issue_mode_instance = issueMode(api_object = github_mock_object,repo_name="test_repo")
+    # First time call comment_issue_mode
+    create_issue_flag = issue_mode_instance.comment_issue_mode(issue_name="Insight",issue_body="hello world")
+    # a new issue should be generated
+    assert create_issue_flag
+    # Second time call comment_issue_mode
+    create_issue_flag = issue_mode_instance.comment_issue_mode(issue_name="Insight",issue_body="hello world")
+
+    # An old issue should be commented and the method should return false
+    assert create_issue_flag == False
