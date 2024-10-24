5dc70be6c2599c5f01efdcd606d1b42101249e9a
Rebekah Rudd <rudd01@allegheny.edu>
2024-10-24 19:15:07 -0400

    chore: added doc strings and deleted extra code in report_params.py

--- gatorgrade/output/report_params.py
+++ gatorgrade/output/report_params.py
@@ -6,32 +6,25 @@ from enum import Enum
 class ReportParamsLocation(str, Enum):
     """Define the location for the parameters of reporting and storing gatorgrade checks."""
 
-    report_location: str
     file = "file"
     env = "env"
-    # none = validate_location()
 
 
 def validate_location(location):
-    """todo."""
+    """Validate the that there is a value in ReportParamsLocation."""
     if location not in ReportParamsLocation:
         raise ValueError("Invalid location for --report-location: {}".format(location))
-
-
-# how to call it
-# validate_location(ReportParamsLocation.report_location)
 
 
 class ReportParamsType(str, Enum):
     """Define the type of type to store the data in."""
 
-    report_storing_type: str
     json = "json"
     md = "md"
 
 
 def validate_storing_type(storing_type):
-    """todo."""
+    """Validate the that there is a value in ReportParamsType."""
     if storing_type not in ReportParamsType:
         raise ValueError(
             "Invalid type for --report-storing-type: {}".format(storing_type)
@@ -41,13 +34,12 @@ def validate_storing_type(storing_type):
 class ReportParamsStoringName(str, Enum):
     """Define the type of type to store the data in."""
 
-    storing_location_name: str
     file: str
     github = "github"
 
 
 def validate_storing_location_name(storing_location_name):
-    """todo."""
+    """Validate the that there is a value in ReportParamsStoringName."""
     if storing_location_name not in ReportParamsStoringName:
         raise ValueError(
             "Invalid type for --report-storing-type: {}".format(storing_location_name)
