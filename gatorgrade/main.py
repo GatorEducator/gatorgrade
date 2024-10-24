7a0281856ce74b43cbc48deb364f13122ae643af
Rebekah Rudd <rudd01@allegheny.edu>
2024-10-24 16:50:43 -0400

    chore: update the validate functions in main.py

--- gatorgrade/main.py
+++ gatorgrade/main.py
@@ -62,8 +62,8 @@ def gatorgrade(
     """Run the GatorGrader checks in the specified gatorgrade.yml file."""
     # first check the report params
     validate_location(ReportParamsLocation.report_location)
-    validate_storing_type(ReportParamsLocation.report_storing_type)
-    validate_storing_location_name(ReportParamsLocation.storing_location_name)
+    validate_storing_type(ReportParamsType.report_storing_type)
+    validate_storing_location_name(ReportParamsStoringName.storing_location_name)
 
     # if ctx.subcommand is None then this means
     # that, by default, gatorgrade should run in checking mode
