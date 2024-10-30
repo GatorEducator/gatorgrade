79a18ece892e781de8f242ae140b0e0fe1917396
Chezka Quinola <quinola01@allegheny.edu>
2024-10-30 12:05:53 -0400

    fix: revert parse_config

--- gatorgrade/main.py
+++ gatorgrade/main.py
@@ -81,7 +81,7 @@ def gatorgrade(
     # that, by default, gatorgrade should run in checking mode
     if ctx.invoked_subcommand is None:
         # parse the provided configuration file
-        (checks, match) = parse_config(filename)
+        (checks, match) = parse_config(filename, check_include, check_exclude)
         # there are valid checks and thus the
         # tool should run them with run_checks
         if len(checks) > 0:
