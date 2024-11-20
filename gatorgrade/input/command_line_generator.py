01c448cc936b6850ee57d04171eb0e797ba1450a
Aidan Dyga <aidan.dyga@gmail.com>
2024-11-20 16:25:38 -0500

    fix(gg_args): add a default check if there is none so the shell checks run

--- gatorgrade/input/command_line_generator.py
+++ gatorgrade/input/command_line_generator.py
@@ -29,8 +29,8 @@ def generate_checks(
         description = check_data.check.get("description")
         if description is not None:
             gg_args.extend(["--description", str(description)])
-        # Add name of check if it exists in data
-        check_name = check_data.check.get("check")
+        # Add name of check if it exists in data, otherwise use default_check
+        check_name = check_data.check.get("check", "default_check")
         if check_name is not None:
             gg_args.append(str(check_name))
         # Add any additional options
