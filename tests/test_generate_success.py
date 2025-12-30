dde4c5c7f804b6b9f177767f98f7354948d8fec3
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-12-30 14:24:52 -0500

    test: Improve the suppression of console output in the tests/test_generate_success.py file.

--- tests/test_generate_success.py
+++ tests/test_generate_success.py
@@ -23,19 +23,23 @@ def setup_files(tmp_path):
     yield tmp_path
 
 
-def test_generate_config_create_gatorgrade_yml(testing_dir):
+def test_generate_config_create_gatorgrade_yml(testing_dir, capsys):
     """Test to see that gatorgrade_yml file exist in file structure"""
     # When generate_config is called
     generate_config(["src"], str(testing_dir))
+    capsys.readouterr()
     # Then gatorgrade.yml is created
     gatorgrade_yml = testing_dir / "gatorgrade.yml"
     assert gatorgrade_yml.is_file()
 
 
-def test_generate_config_creates_gatorgrade_yml_with_dir_in_user_input(testing_dir):
+def test_generate_config_creates_gatorgrade_yml_with_dir_in_user_input(
+    testing_dir, capsys
+):
     """Test to see if input matches directory"""
     # When generate_config is called
     generate_config(["src"], str(testing_dir))
+    capsys.readouterr()
     # Then gatorgrade.yml is created
     gatorgrade_yml = testing_dir / "gatorgrade.yml"
     assert "src/test.py" in gatorgrade_yml.open().read()
@@ -43,10 +47,12 @@ def test_generate_config_creates_gatorgrade_yml_with_dir_in_user_input(testing_d
 
 def test_generate_config_creates_gatorgrade_yml_without_dir_not_in_user_input(
     testing_dir,
+    capsys,
 ):
     """Test to see if input does not match directory"""
     # When generate_config is called
     generate_config(["writing"], str(testing_dir))
+    capsys.readouterr()
     # Then gatorgrade.yml is created
     gatorgrade_yml = testing_dir / "gatorgrade.yml"
     assert "src/test.py" not in gatorgrade_yml.open().read()
