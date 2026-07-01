569f7f5858adc36b8902e56d0f61b51e0a7f9988
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2026-07-01 16:50:31 -0400

    coms: Add a comment as a reminder about the order of the strings inside of th DUE_DATE_ALIASES tuple in gatorgrade/input/parse_config.py.

--- gatorgrade/input/parse_config.py
+++ gatorgrade/input/parse_config.py
@@ -14,6 +14,13 @@ from gatorgrade.input.in_file_path import (
     reformat_yaml_data,
 )
 
+# IMPORTANT: note that the ordering of the front-matter field
+# names inside of the DUE_DATE_ALIASES tuple is important because
+# the first alias to match is the one that will be extracted;
+# using the due_date alias first means that it will be the
+# one given priority if more than one is found in the front-matter
+# of a gatorgrade configuration file
+
 NAME_FIELD = "name"
 DUE_DATE_FIELD = "due_date"
 DUE_DATE_ALIASES = ("due_date", "duedate", "due", "date")
