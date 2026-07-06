73f2a88325b169ac09bbed9d583f9ab378777e87
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2026-07-06 17:11:30 -0400

    coms: Further improve the docstring in the gatorgrade/__init__.py file.

--- gatorgrade/__init__.py
+++ gatorgrade/__init__.py
@@ -1,1 +1,1 @@
-"""GatorGrade is a program that checks your work according to gatorgrader checks."""
+"""GatorGrade is a program that checks your work according to gatorgrader-drive and shell-based checks."""
