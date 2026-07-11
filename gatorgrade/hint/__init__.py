9dabcd008a9c6e36a703ef17800b1846daef1652
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2026-07-11 11:26:46 -0400

    refactor: Make the truncation parameters centrally located in the support module in the gatorgrade/hint/__init__.py file.

--- gatorgrade/hint/__init__.py
+++ gatorgrade/hint/__init__.py
@@ -3,7 +3,6 @@
 from gatorgrade.hint.local_engine import (
     DEFAULT_MODEL_ID,
     ENV_CACHE_DIR,
-    HINT_FILE_LINES,
     HINT_MAX_TOKENS,
     HINT_REPETITION_PENALTY,
     HINT_TEMPERATURE,
@@ -12,26 +11,27 @@ from gatorgrade.hint.local_engine import (
 )
 from gatorgrade.hint.remote_engine import (
     REMOTE_API_KEY_DEFAULT,
-    REMOTE_HINT_DIAG_TRUNCATE,
-    REMOTE_HINT_FILE_LINES,
     REMOTE_HINT_MAX_TOKENS,
     REMOTE_HINT_TEMPERATURE,
     REMOTE_HINT_TIMEOUT_MS,
     REMOTE_MODEL_DEFAULT,
     RemoteHintEngine,
 )
+from gatorgrade.hint.support import (
+    HINT_DIAG_TRUNCATE,
+    HINT_FILE_LINES,
+)
 
 __all__ = [
     "DEFAULT_MODEL_ID",
     "ENV_CACHE_DIR",
+    "HINT_DIAG_TRUNCATE",
     "HINT_FILE_LINES",
     "HINT_MAX_TOKENS",
     "HINT_REPETITION_PENALTY",
     "HINT_TEMPERATURE",
     "HINT_TOP_P",
     "REMOTE_API_KEY_DEFAULT",
-    "REMOTE_HINT_DIAG_TRUNCATE",
-    "REMOTE_HINT_FILE_LINES",
     "REMOTE_HINT_MAX_TOKENS",
     "REMOTE_HINT_TEMPERATURE",
     "REMOTE_HINT_TIMEOUT_MS",
