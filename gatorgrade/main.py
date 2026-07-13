7cc2ce48790516a768785712a5959d2f347b19ae
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2026-07-13 11:01:13 -0400

    fix: Use the word "path" instead of "file" in gatorgrade/main.py as it is may be a file or a directory at this stage.

--- gatorgrade/main.py
+++ gatorgrade/main.py
@@ -544,7 +544,7 @@ def gatorgrade(  # noqa: PLR0912, PLR0913, PLR0915
             console.print(Rule(CONFIG_ERROR_LABEL, style="bright_red"))
             console.print()
             console.print(
-                f"The file {resolved_filename} either does not exist or is not valid."
+                f"The path {resolved_filename} either does not exist or is not valid."
             )
             console.print(Text(EXIT_MESSAGE))
             console.print()
