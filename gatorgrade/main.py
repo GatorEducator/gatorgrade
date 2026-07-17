eb18f7f4ef10ae06fb583ae9b1c41d4159da5d34
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2026-07-17 17:01:23 -0400

    fix: Remove a not-needed blank space for a help message in the gatorgrade/main.py file.

--- gatorgrade/main.py
+++ gatorgrade/main.py
@@ -281,7 +281,7 @@ def gatorgrade(  # noqa: PLR0912, PLR0913, PLR0915
             "Field to match the filter query against. DESCRIPTION"
             " filters on check description; NAME filters on"
             " check's name (or shell command for command-only"
-            " checks); HINT filters on check's hint; ANY "
+            " checks); HINT filters on check's hint; ANY"
             " filters on all three fields."
         ),
         show_default=True,
